99797363c35b75eedbc145752412929416f92014
Mark Kurtz <mark@neuralmagic.com>
2019-08-30 09:50:37 -0400

    remove issue with plugin for fatrelu where threshold was defaulted to a high value

--- scripts/plugins/model_set_fat_relus.py
+++ scripts/plugins/model_set_fat_relus.py
@@ -5,7 +5,7 @@ from neuralmagicML.sparsity import convert_relus_to_fat
 
 
 def edit_model(model: Module, model_tag: Union[None, str]):
-    converted = convert_relus_to_fat(model, inplace=True, threshold=0.05)
+    converted = convert_relus_to_fat(model, inplace=True)
     print('converted {} relus to FAT relus'.format(len(converted)))
 
     return model
