3876d0b7e410e6491eadbf294ced6599332aba73
Mark Kurtz <mark@neuralmagic.com>
2020-02-16 10:35:24 -0500

    minor reformatting

--- tests/recal/kernel/test_sensitivity.py
+++ tests/recal/kernel/test_sensitivity.py
@@ -7,20 +7,47 @@ from torch.nn import Sequential, Linear, ReLU, Conv2d
 import torch.nn.functional as TF
 from torch.utils.data import Dataset
 
-from neuralmagicML.recal import ModuleParamKSSensitivity, KSSensitivityProgress, ModuleKSSensitivityAnalysis
+from neuralmagicML.recal import (
+    ModuleParamKSSensitivity,
+    KSSensitivityProgress,
+    ModuleKSSensitivityAnalysis,
+)
 from neuralmagicML.utils import LossWrapper
 
 
 @pytest.mark.parametrize(
-    'name,param_name,type_,execution_order,measured,expected_integral',
+    "name,param_name,type_,execution_order,measured,expected_integral",
     [
-        ('layer.name', 'weight', 'linear', 0, [(0.05, 0.0), (0.25, 0.0), (0.5, 0.0), (0.75, 0.0), (0.95, 0.0)], 0.0),
-        ('layer.name', 'weight', 'conv', 0, [(0.05, 1.0), (0.25, 1.0), (0.5, 1.0), (0.75, 1.0), (0.95, 1.0)], 1.0),
-        ('layer.name', 'weight', 'conv', 0, [(0.05, 1.0), (0.25, 1.5), (0.5, 2.0), (0.75, 3.75), (0.95, 6.0)], 2.7312498092651367),
-    ]
+        (
+            "layer.name",
+            "weight",
+            "linear",
+            0,
+            [(0.05, 0.0), (0.25, 0.0), (0.5, 0.0), (0.75, 0.0), (0.95, 0.0)],
+            0.0,
+        ),
+        (
+            "layer.name",
+            "weight",
+            "conv",
+            0,
+            [(0.05, 1.0), (0.25, 1.0), (0.5, 1.0), (0.75, 1.0), (0.95, 1.0)],
+            1.0,
+        ),
+        (
+            "layer.name",
+            "weight",
+            "conv",
+            0,
+            [(0.05, 1.0), (0.25, 1.5), (0.5, 2.0), (0.75, 3.75), (0.95, 6.0)],
+            2.7312498092651367,
+        ),
+    ],
 )
 @pytest.mark.device_cpu
-def test_module_param_ks_sensitivity(name, param_name, type_, execution_order, measured, expected_integral):
+def test_module_param_ks_sensitivity(
+    name, param_name, type_, execution_order, measured, expected_integral
+):
     sens = ModuleParamKSSensitivity(name, param_name, type_, execution_order, measured)
     assert sens.name == name
     assert sens.param_name == param_name
@@ -30,14 +57,38 @@ def test_module_param_ks_sensitivity(name, param_name, type_, execution_order, m
 
 
 @pytest.mark.parametrize(
-    'layer_index,layer_name,layers,sparsity_index,sparsity_levels,measurement_step,samples_per_measurement',
+    "layer_index,layer_name,layers,sparsity_index,sparsity_levels,measurement_step,samples_per_measurement",
     [
-        (0, 'layer.name', ['layer.name', 'layer2.name'], 0, [0.2, 0.5, 0.7, 0.9, 0.95], 0, 100)
-    ]
+        (
+            0,
+            "layer.name",
+            ["layer.name", "layer2.name"],
+            0,
+            [0.2, 0.5, 0.7, 0.9, 0.95],
+            0,
+            100,
+        )
+    ],
 )
 @pytest.mark.device_cpu
-def test_ks_sensitivity_progress(layer_index, layer_name, layers, sparsity_index, sparsity_levels, measurement_step, samples_per_measurement):
-    progress = KSSensitivityProgress(layer_index, layer_name, layers, sparsity_index, sparsity_levels, measurement_step, samples_per_measurement)
+def test_ks_sensitivity_progress(
+    layer_index,
+    layer_name,
+    layers,
+    sparsity_index,
+    sparsity_levels,
+    measurement_step,
+    samples_per_measurement,
+):
+    progress = KSSensitivityProgress(
+        layer_index,
+        layer_name,
+        layers,
+        sparsity_index,
+        sparsity_levels,
+        measurement_step,
+        samples_per_measurement,
+    )
     assert progress.layer_index == layer_index
     assert progress.layer_name == layer_name
     assert progress.layers == layers
@@ -48,13 +99,7 @@ def test_ks_sensitivity_progress(layer_index, layer_name, layers, sparsity_index
 
 
 TEST_MODEL = Sequential(
-    Linear(8, 16),
-    ReLU(),
-    Linear(16, 32),
-    ReLU(),
-    Linear(32, 64),
-    ReLU(),
-    Linear(64, 1)
+    Linear(8, 16), ReLU(), Linear(16, 32), ReLU(), Linear(32, 64), ReLU(), Linear(64, 1)
 )
 
 
@@ -96,7 +141,9 @@ class TestingCNNDataset(Dataset):
         return self._length
 
 
-def _test_module_ks_sensitivity_analysis_one_shot(model, dataset, loss_fn, batch_size, samples_per_measurement, device):
+def _test_module_ks_sensitivity_analysis_one_shot(
+    model, dataset, loss_fn, batch_size, samples_per_measurement, device
+):
     analysis = ModuleKSSensitivityAnalysis(model, dataset, loss_fn)
     progress_counter = 0
 
@@ -107,7 +154,11 @@ def _test_module_ks_sensitivity_analysis_one_shot(model, dataset, loss_fn, batch
     handle = analysis.register_progress_hook(_progress_hook)
     results = analysis.run_one_shot(device, batch_size, samples_per_measurement)
     assert len(results) > 0
-    expected = ((1 + math.ceil(samples_per_measurement / batch_size)) * len(results[-1].measured) + 1) * len(results)
+    expected = (
+        (1 + math.ceil(samples_per_measurement / batch_size))
+        * len(results[-1].measured)
+        + 1
+    ) * len(results)
     assert expected == progress_counter
     handle.remove()
 
@@ -116,24 +167,32 @@ def _test_module_ks_sensitivity_analysis_one_shot(model, dataset, loss_fn, batch
 
 
 @pytest.mark.parametrize(
-    'model,dataset,loss_fn,batch_size,samples_per_measurement',
+    "model,dataset,loss_fn,batch_size,samples_per_measurement",
     [
         (TEST_MODEL, TestingDataset(1000), LossWrapper(TF.mse_loss), 16, 100),
         (TEST_CNN_MODEL, TestingCNNDataset(1000), LossWrapper(TF.mse_loss), 16, 100),
-    ]
+    ],
 )
 @pytest.mark.device_cpu
-def test_module_ks_sensitivity_analysis_one_shot(model, dataset, loss_fn, batch_size, samples_per_measurement):
-    _test_module_ks_sensitivity_analysis_one_shot(model, dataset, loss_fn, batch_size, samples_per_measurement, 'cpu')
+def test_module_ks_sensitivity_analysis_one_shot(
+    model, dataset, loss_fn, batch_size, samples_per_measurement
+):
+    _test_module_ks_sensitivity_analysis_one_shot(
+        model, dataset, loss_fn, batch_size, samples_per_measurement, "cpu"
+    )
 
 
 @pytest.mark.parametrize(
-    'model,dataset,loss_fn,batch_size,samples_per_measurement',
+    "model,dataset,loss_fn,batch_size,samples_per_measurement",
     [
         (TEST_MODEL, TestingDataset(1000), LossWrapper(TF.mse_loss), 16, 100),
         (TEST_CNN_MODEL, TestingCNNDataset(1000), LossWrapper(TF.mse_loss), 16, 100),
-    ]
+    ],
 )
 @pytest.mark.device_cuda
-def test_module_ks_sensitivity_analysis_one_shot_cuda(model, dataset, loss_fn, batch_size, samples_per_measurement):
-    _test_module_ks_sensitivity_analysis_one_shot(model, dataset, loss_fn, batch_size, samples_per_measurement, 'cuda')
+def test_module_ks_sensitivity_analysis_one_shot_cuda(
+    model, dataset, loss_fn, batch_size, samples_per_measurement
+):
+    _test_module_ks_sensitivity_analysis_one_shot(
+        model, dataset, loss_fn, batch_size, samples_per_measurement, "cuda"
+    )
