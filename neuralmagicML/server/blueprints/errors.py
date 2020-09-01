b2af6dbe6c3181a8e148471fb7b2142535dc0e63
Mark Kurtz <mark@neuralmagic.com>
2020-09-01 19:30:08 -0400

    server errors docs and fixes
    Reviewers: ben, kevinaer, mgoin, alexm, tuan
    
    Reviewed By: ben, kevinaer
    
    Subscribers: #core, #python_ml
    
    Differential Revision: https://phabricator.neuralmagic.com:8021/D2373

--- neuralmagicML/server/blueprints/errors.py
+++ neuralmagicML/server/blueprints/errors.py
@@ -10,7 +10,7 @@ from werkzeug.exceptions import BadRequest
 from flask import Blueprint, jsonify
 
 from neuralmagicML.server.blueprints.helpers import HTTPNotFoundError
-from neuralmagicML.server.schemas import ErrorSchema
+from neuralmagicML.server.schemas import data_dump_and_validation, ErrorSchema
 
 __all__ = ["errors_blueprint"]
 
@@ -23,26 +23,51 @@ errors_blueprint = Blueprint("errors", __name__)
 @errors_blueprint.app_errorhandler(ValidationError)
 @errors_blueprint.app_errorhandler(BadRequest)
 def handle_client_error(error: ValidationError):
-    _LOGGER.error("handling client error, returning 400 status: {}".format(error))
-    error_obj = {"error_type": error.__class__.__name__, "error_message": str(error)}
-    response = ErrorSchema().dump(error_obj)
+    """
+    Handle an error that occurred in the flask app that should return a 400 response
 
-    return jsonify(response), HTTPStatus.BAD_REQUEST
+    :param error: the error that occurred
+    :return: a tuple containing (json response, http status code [400])
+    """
+    _LOGGER.error("handling client error, returning 400 status: {}".format(error))
+    resp_error = data_dump_and_validation(
+        ErrorSchema(),
+        {"error_type": error.__class__.__name__, "error_message": str(error)},
+    )
+
+    return jsonify(resp_error), HTTPStatus.BAD_REQUEST
 
 
 @errors_blueprint.app_errorhandler(HTTPNotFoundError)
 def handle_client_error(error: HTTPNotFoundError):
-    _LOGGER.error("handling not found error, returning 404 status: {}".format(error))
-    error_obj = {"error_type": error.__class__.__name__, "error_message": str(error)}
-    response = ErrorSchema().dump(error_obj)
+    """
+    Handle an error that occurred in the flask app that should return a 404 response
 
-    return jsonify(response), HTTPStatus.NOT_FOUND
+    :param error: the error that occurred
+    :return: a tuple containing (json response, http status code [404])
+    """
+    _LOGGER.error("handling not found error, returning 404 status: {}".format(error))
+    resp_error = data_dump_and_validation(
+        ErrorSchema(),
+        {"error_type": error.__class__.__name__, "error_message": str(error)},
+    )
+
+    return jsonify(resp_error), HTTPStatus.NOT_FOUND
 
 
 @errors_blueprint.app_errorhandler(Exception)
 def handle_unexpected_error(error: Exception):
-    _LOGGER.error("handling unexpected error, returning 500 status: {}".format(error))
-    error_obj = {"error_type": error.__class__.__name__, "error_message": str(error)}
-    response = ErrorSchema().dump(error_obj)
+    """
+    Handle an error that occurred in the flask app that was not expected.
+    Will return as a 500 representing a server error, 500
 
-    return jsonify(response), HTTPStatus.INTERNAL_SERVER_ERROR
+    :param error: the error that occurred
+    :return: a tuple containing (json response, http status code [500])
+    """
+    _LOGGER.error("handling unexpected error, returning 500 status: {}".format(error))
+    resp_error = data_dump_and_validation(
+        ErrorSchema(),
+        {"error_type": error.__class__.__name__, "error_message": str(error)},
+    )
+
+    return jsonify(resp_error), HTTPStatus.INTERNAL_SERVER_ERROR
