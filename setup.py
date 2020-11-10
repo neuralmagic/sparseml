23b5167fd16656f19e7fa66dd7698cf390030cc9
Mark Kurtz <mark@neuralmagic.com>
2020-11-10 12:50:31 -0500

    remove linux requirements from python versions older than 3.6
    Reviewers: ben, kevinaer, alexm, tuan, mgoin
    
    Reviewed By: ben, kevinaer
    
    Subscribers: #core, #python_ml
    
    Differential Revision: https://phabricator.neuralmagic.com:8021/D2658

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import sys
 from sys import platform
 
 packages = find_packages(
@@ -8,18 +9,20 @@ packages = find_packages(
 
 def get_reqs(req_file_path):
     with open(req_file_path, "r") as req_file:
-
-        install_reqs = []
+        reqs = []
 
         for line in req_file.readlines():
             if line.strip() and not line.startswith("#"):
-                install_reqs.append(line)
-        return install_reqs
+                reqs.append(line)
+
+        return reqs
 
 
 install_reqs = get_reqs("requirements.txt")
 
-if platform == "linux" or platform == "linux2":
+if (platform == "linux" or platform == "linux2") and sys.version >= "3.6":
+    # we check the python version because linux specific requirements
+    # generally are not built for older / EOL python versions such as 3.5
     install_reqs += get_reqs("requirements-linux.txt")
 
 setup(
@@ -33,7 +36,5 @@ setup(
     author_email="support@neuralmagic.com",
     url="https://neuralmagic.com/",
     license_file="LICENSE",
-    entry_points={
-        "console_scripts": ["sparsify=neuralmagicML.server.app:main"],
-    },
+    entry_points={"console_scripts": ["sparsify=neuralmagicML.server.app:main"]},
 )
