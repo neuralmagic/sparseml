e47a09337ef32a05ced3bfd3ad9e2252d66a2b5b
Tuan Nguyen <tuan@neuralmagic.com>
2021-03-06 11:49:39 -0500

    Remove masks from SavedModel format; unittest for save/load models with masked layers

--- tests/sparseml/keras/optim/test_mask_pruning.py
+++ tests/sparseml/keras/optim/test_mask_pruning.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import math
+import os
+import tempfile
 from typing import Dict, Union
 
 import numpy as np
@@ -22,6 +25,7 @@ from sparseml.keras.optim import (
     GMPruningModifier,
     MaskedLayer,
     ScheduledModifierManager,
+    remove_pruning_masks
 )
 from tests.sparseml.keras.optim.mock import (
     DenseLayerCreator,
@@ -180,6 +184,89 @@ def test_nested_layer_structure(modifier_lambdas, steps_per_epoch):
     assert model_config == new_model.get_config()
 
     tf.keras.backend.clear_session()
+
+
+@pytest.mark.parametrize(
+    "modifier_lambdas",
+    [
+        (
+            lambda: GMPruningModifier(
+                params=["conv2d/kernel:0"],
+                init_sparsity=0.25,
+                final_sparsity=0.75,
+                start_epoch=0.0,
+                end_epoch=2.0,
+                update_frequency=1.0,
+            ),
+            lambda: GMPruningModifier(
+                params=["conv2d_1/kernel:0"],
+                init_sparsity=0.25,
+                final_sparsity=0.75,
+                start_epoch=0.0,
+                end_epoch=2.0,
+                update_frequency=1.0,
+            ),
+        ),
+    ],
+    scope="function",
+)
+@pytest.mark.parametrize("epochs", [2], scope="function")
+@pytest.mark.parametrize("batch_size", [64], scope="function")
+def test_save_load_masked_model(modifier_lambdas, epochs, batch_size):
+    # Data
+    num_classes = 10
+    (x_train, y_train), _ = tf.keras.datasets.mnist.load_data()
+    y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+
+    N = 2 * batch_size
+    x_train = x_train[:N, :]
+    y_train = y_train[:N, :]
+
+    # Model
+    model = mnist_model()
+    modifiers = [mod() for mod in modifier_lambdas]
+    manager = ScheduledModifierManager(modifiers)
+    optimizer = tf.keras.optimizers.Adam()
+    steps_per_epoch = math.ceil(len(x_train) / batch_size)
+    model, optimizer, callbacks = manager.modify(model, optimizer, steps_per_epoch)
+    model.compile(
+        loss=tf.keras.losses.categorical_crossentropy,
+        optimizer=optimizer,
+        metrics=["accuracy"],
+        run_eagerly=True,
+    )
+    model.fit(
+        x_train, y_train, batch_size=batch_size, epochs=epochs, callbacks=callbacks
+    )
+
+    # Verify the model can be saved and loaded again with the same weights
+    with tempfile.TemporaryDirectory() as save_dir:
+        checkpoint_filepath = os.path.join(save_dir, "model.tf")
+        model.save(checkpoint_filepath)
+        new_model = tf.keras.models.load_model(checkpoint_filepath)
+
+        # Verify two models are different objects
+        assert id(model) != id(new_model)
+
+        # Verify two models have the same weights each layer
+        _assert_equal_models(model, new_model)
+
+    # Verify removing masked layers
+    model = remove_pruning_masks(model)
+    mask_count = len([layer for layer in model.layers if isinstance(layer, MaskedLayer)])
+    assert mask_count == 0
+
+
+def _assert_equal_models(model: tf.keras.Model, new_model: tf.keras.Model):
+    for layer, new_layer in zip(model.layers, new_model.layers):
+        weights, new_weights = layer.get_weights(), new_layer.get_weights()
+        assert len(weights) == len(new_weights)
+        for w, new_w in zip(weights, new_weights):
+            # Verify two weights are different objects
+            assert id(w) != id(new_w)
+
+            # Verify they have the same values
+            assert np.array_equal(w, new_w)
 
 
 def _count_nested_masked_layers_in_config(layer_config: Dict):
