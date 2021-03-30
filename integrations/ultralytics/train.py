c792c2e8568b96a9d5a6b4ebc4955011f7d7c054
Benjamin <ben@neuralmagic.com>
2021-03-30 13:43:43 -0400

    YOLOv3 integration sparsezoo weights fixes

--- integrations/ultralytics/train.py
+++ integrations/ultralytics/train.py
@@ -104,7 +104,7 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
     assert len(names) == nc, '%g names found for nc=%g dataset in %s' % (len(names), nc, opt.data)  # check
 
     # Model
-    pretrained = weights.endswith('.pt')
+    pretrained = weights.endswith('.pt') or weights.endswith('.pth')  # SparseML integration
     if pretrained:
         with torch_distributed_zero_first(rank):
             attempt_download(weights)  # download if not found locally
@@ -643,14 +643,14 @@ if __name__ == '__main__':
                 "Attempting to load weights from SparseZoo recipe, but not given a "
                 "SparseZoo recipe stub.  When --weights is set to 'zoo'. "
                 "sparseml-recipe must start with 'zoo:' and be a SparseZoo model "
-                f"stub. sparseml-recipe was set to {args.sparseml_recipe}"
+                f"stub. sparseml-recipe was set to {opt.sparseml_recipe}"
             )
     elif opt.weights.startswith("zoo:"):
         # Load weights from a SparseZoo model stub
         zoo_model = Zoo.load_model_from_stub(opt.weights)
-        args.initial_checkpoint = zoo_model.download_framework_files(
+        opt.weights = zoo_model.download_framework_files(
             extensions=[".pt", ".pth"]
-        )
+        )[0]
     ####################################################################################
     # End - SparseML optional load weights from SparseZoo
     ####################################################################################
