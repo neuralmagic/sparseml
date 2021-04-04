47b282428fe0b35a497b73223c143bf899c9c31d
Mark Kurtz <mark@neuralmagic.com>
2021-04-03 23:32:07 -0400

    changes to enable multiversion and neural magic styling for the docs builds

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ author = "Neural Magic"
 version = "unknown"
 version_major_minor = version
 # load and overwrite version info from sparseml package
-exec(open(os.path.join(os.pardir, os.pardir, "src", "sparseml", "version.py")).read())
+exec(open(os.path.join(os.pardir, "src", "sparseml", "version.py")).read())
 release = version
 version = version_major_minor
 print(f"loaded versions from src/sparseml/version.py and set to {release}, {version}")
