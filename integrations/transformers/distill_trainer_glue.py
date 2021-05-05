fix loss calculation error

integrations/transformers/distill_trainer_glue.py
@@ -48,7 +48,7 @@ class DistillGlueTrainer(Trainer):
         """
         input_device = inputs["input_ids"].device
         outputs = model(**inputs)
-        loss = torch.mean(outputs['loss'])
+        loss = outputs['loss']
         if self.teacher is not None:
             self.teacher = self.teacher.to(input_device)
             student_logit_neg = F.softmax(outputs['logits'][:, :1]/ self.temperature, dim=-1)