149b8d13a4a0a9673fc3a183e3bf24a0925fb740
Mark Kurtz <mark@neuralmagic.com>
2021-06-10 18:18:29 -0400

    Add PyTorch layer pruning modifier (#278)
    * Add PyTorch layer pruning modifier
    
    * fix quality and address comments
    
    * add example in docs for layer pruning modifier
    
    Co-authored-by: Mark Kurtz <mark.kurtz@neuralmagic.com>

--- tests/sparseml/pytorch/optim/test_modifier_pruning.py
+++ tests/sparseml/pytorch/optim/test_modifier_pruning.py
@@ -18,15 +18,18 @@ import pytest
 import torch
 
 from flaky import flaky
+from sparseml.pytorch.nn import Identity
 from sparseml.pytorch.optim import (
     ConstantPruningModifier,
     GlobalMagnitudePruningModifier,
     GMPruningModifier,
+    LayerPruningModifier,
     MagnitudePruningModifier,
     MovementPruningModifier,
     load_mask_creator,
 )
-from tests.sparseml.pytorch.helpers import LinearNet
+from sparseml.pytorch.utils import get_layer
+from tests.sparseml.pytorch.helpers import FlatMLPNet, LinearNet
 from tests.sparseml.pytorch.optim.test_modifier import (
     ScheduledModifierTest,
     ScheduledUpdateModifierTest,
@@ -318,6 +321,97 @@ class TestGMPruningModifier(ScheduledUpdateModifierTest):
             test_steps_per_epoch,
             True,
         )
+
+
+@pytest.mark.parametrize(
+    "modifier_lambda",
+    [
+        lambda: LayerPruningModifier(
+            layers=["seq.fc2", "seq.act2"],
+            start_epoch=0.0,
+            end_epoch=15.0,
+            update_frequency=1.0,
+        ),
+        lambda: LayerPruningModifier(
+            layers=["seq.fc2", "seq.act2"],
+            start_epoch=10.0,
+            end_epoch=25.0,
+            update_frequency=1.0,
+        ),
+        lambda: LayerPruningModifier(
+            layers="__ALL_PRUNABLE__",
+            start_epoch=10.0,
+            end_epoch=25.0,
+        ),
+    ],
+    scope="function",
+)
+@pytest.mark.parametrize("model_lambda", [FlatMLPNet], scope="function")
+@pytest.mark.parametrize(
+    "optim_lambda",
+    [create_optim_sgd, create_optim_adam],
+    scope="function",
+)
+class TestLayerPruningModifier(ScheduledUpdateModifierTest):
+    def test_lifecycle(
+        self,
+        modifier_lambda,
+        model_lambda,
+        optim_lambda,
+        test_steps_per_epoch,  # noqa: F811
+    ):
+        modifier = modifier_lambda()
+        model = model_lambda()
+        optimizer = optim_lambda(model)
+        self.initialize_helper(modifier, model)
+        assert len(modifier._layer_modules) > 0
+        if modifier.start_epoch > 0:
+            for (name, mod) in modifier._layer_modules.items():
+                assert mod is None
+                assert not isinstance(get_layer(name, model), Identity)
+
+            # check sparsity is not set before
+            for epoch in range(int(modifier.start_epoch)):
+                assert not modifier.update_ready(epoch, test_steps_per_epoch)
+
+            for (name, mod) in modifier._layer_modules.items():
+                assert mod is None
+                assert not isinstance(get_layer(name, model), Identity)
+
+            epoch = int(modifier.start_epoch)
+            assert modifier.update_ready(epoch, test_steps_per_epoch)
+            modifier.scheduled_update(model, optimizer, epoch, test_steps_per_epoch)
+        else:
+            epoch = 0
+
+        for (name, mod) in modifier._layer_modules.items():
+            assert mod is not None
+            assert isinstance(get_layer(name, model), Identity)
+
+        # check forward pass
+        input_shape = model_lambda.layer_descs()[0].input_size
+        test_batch = torch.randn(10, *input_shape)
+        _ = model(test_batch)
+
+        end_epoch = (
+            modifier.end_epoch if modifier.end_epoch > -1 else modifier.start_epoch + 10
+        )
+
+        while epoch < end_epoch - 0.1:
+            epoch += 0.1
+            assert modifier.update_ready(epoch, test_steps_per_epoch)
+            modifier.scheduled_update(model, optimizer, epoch, test_steps_per_epoch)
+
+        _ = model(test_batch)  # check forward pass
+
+        if modifier.end_epoch > -1:
+            epoch = int(modifier.end_epoch)
+            assert modifier.update_ready(epoch, test_steps_per_epoch)
+            modifier.scheduled_update(model, optimizer, epoch, test_steps_per_epoch)
+
+            for (name, mod) in modifier._layer_modules.items():
+                assert mod is None
+                assert not isinstance(get_layer(name, model), Identity)
 
 
 @pytest.mark.skipif(
