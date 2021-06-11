ef782b8597e4b0dbb67283051ca5b325cbee9dd7
Benjamin Fineran <bfineran@users.noreply.github.com>
2021-06-11 14:31:13 -0400

    PyTorch DDP support for gradient based pruning methods (#269)
    * PyTorch DDP support for gradient based pruning methods
    
    * fix MFAC gather step bug
    
    * fix MFAC gather step bug
    
    * fix GPU memory leak in block MFAC computation
    
    * fix end step for non DDP MFAC pruning
    
    * move MFAC grad buffer initialization to after training starts
    
    * initialize grad buffer after grads exist
    
    * do not include DDP info when pickling grad-scorers
    
    * add attribute check for DDP set state check
    
    * free GPU mem in between parallel MFAC block computations
    
    * exclude any MFAC grad or DDP variables from pickle and deepcopy save/load
    
    * improve GPU memory usage for block H_inv diag and mul ops
    
    * run pruning end tasks after final pruning step is made

--- src/sparseml/pytorch/optim/modifier_pruning.py
+++ src/sparseml/pytorch/optim/modifier_pruning.py
@@ -750,14 +750,6 @@ class GMPruningModifier(_PruningParamsModifier):
             self._module_masks.enabled = True
             started = True
 
-        if self.end_pending(epoch, steps_per_epoch):
-            # prune weights to final sparsity if not already
-            self._module_masks.apply()
-            # disable weight reintroduction if not already
-            self._module_masks.disable_reintroduction()
-            if not self._leave_enabled:
-                self._module_masks.enabled = False
-
         self._module_masks.pre_optim_step_update()
         self._pre_step_completed = True
 
@@ -772,6 +764,9 @@ class GMPruningModifier(_PruningParamsModifier):
                 self._inter_func,
             )
             self._module_masks.set_param_masks_from_sparsity(self._applied_sparsity)
+
+        if self.end_pending(epoch, steps_per_epoch):
+            self._module_masks.pruning_end(self._leave_enabled)
 
     def _should_log(
         self, module: Module, optimizer: Optimizer, epoch: float, steps_per_epoch: int
