41c208ed0481bd238f37bf5908965eb9686955c6
spacemanidol <dcampos3@illinois.edu>
2021-06-21 13:24:36 -0500

    adding to readme for baseline

--- research/information_retrieval/doc2query/make_index.sh
+++
@@ -1,6 +0,0 @@
-python -m pyserini.index -collection JsonCollection \
-                         -generator DefaultLuceneDocumentGenerator \
-                         -threads 16 \
-                         -input data/augmented_collection.jsonl \
-                         -index data/indexes/msmarco_jsonl \
-                         -storePositions -storeDocvectors -storeRaw
