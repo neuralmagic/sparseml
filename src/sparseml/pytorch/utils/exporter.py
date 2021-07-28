48cd987f47a98e52111c1d7e8ef00b8a3cb03eaa
Mark Kurtz <mark.kurtz@neuralmagic.com>
2021-07-28 11:49:40 -0400

    Change to make more generic to accept kwargs for the torch.onnx.export api

--- src/sparseml/pytorch/utils/exporter.py
+++ src/sparseml/pytorch/utils/exporter.py
@@ -176,7 +176,7 @@ class ModuleExporter(object):
         opset: int = DEFAULT_ONNX_OPSET,
         disable_bn_fusing: bool = True,
         convert_qat: bool = False,
-        dynamic_axes: Optional[Dict] = None,
+        **export_kwargs,
     ):
         """
         Export an onnx file for the current module and for a sample batch.
@@ -198,10 +198,14 @@ class ModuleExporter(object):
             the module being exported, the resulting QAT ONNX model will be converted
             to a fully quantized ONNX model using `quantize_torch_qat_export`. Default
             is False.
-        :param dynamic_axes: The axes to export with a generic placeholder to signify
-            a dynamic shape instead of static. Generally used for batch sizes
-            to enable dynamic batch inputs for ONNX graphs.
+        :param export_kwargs: kwargs to be passed as is to the torch.onnx.export api
+            call. Useful to pass in dyanmic_axes, input_names, output_names, etc.
+            See more on the torch.onnx.export api spec in the PyTorch docs:
+            https://pytorch.org/docs/stable/onnx.html
         """
+        if not export_kwargs:
+            export_kwargs = {}
+
         if isinstance(sample_batch, Dict) and not isinstance(
             sample_batch, collections.OrderedDict
         ):
@@ -221,20 +225,26 @@ class ModuleExporter(object):
                 sample_batch, self._module, check_feat_lab_inp=False
             )
 
-        input_names = None
-        if isinstance(sample_batch, Tensor):
-            input_names = ["input"]
-        elif isinstance(sample_batch, Dict):
-            input_names = list(sample_batch.keys())
-            sample_batch = tuple([sample_batch[f] for f in input_names])
-        elif isinstance(sample_batch, Iterable):
-            input_names = [
-                "input_{}".format(index) for index, _ in enumerate(iter(sample_batch))
-            ]
-            if isinstance(sample_batch, List):
-                sample_batch = tuple(sample_batch)  # torch.onnx.export requires tuple
+        if "input_names" not in export_kwargs:
+            if isinstance(sample_batch, Tensor):
+                export_kwargs["input_names"] = ["input"]
+            elif isinstance(sample_batch, Dict):
+                export_kwargs["input_names"] = list(sample_batch.keys())
+                sample_batch = tuple(
+                    [sample_batch[f] for f in export_kwargs["input_names"]]
+                )
+            elif isinstance(sample_batch, Iterable):
+                export_kwargs["input_names"] = [
+                    "input_{}".format(index)
+                    for index, _ in enumerate(iter(sample_batch))
+                ]
+                if isinstance(sample_batch, List):
+                    sample_batch = tuple(
+                        sample_batch
+                    )  # torch.onnx.export requires tuple
 
-        output_names = self.get_output_names(out)
+        if "output_names" not in export_kwargs:
+            export_kwargs["output_names"] = self.get_output_names(out)
 
         # disable active quantization observers because they cannot be exported
         disabled_observers = []
@@ -263,12 +273,10 @@ class ModuleExporter(object):
             export_module,
             sample_batch,
             onnx_path,
-            input_names=input_names,
-            output_names=output_names,
             strip_doc_string=True,
             verbose=False,
             opset_version=opset,
-            dynamic_axes=dynamic_axes,
+            **export_kwargs,
         )
 
         # re-enable disabled quantization observers
