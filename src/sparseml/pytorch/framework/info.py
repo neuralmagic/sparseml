6c44437a29316ea70c73c1d9e0b1091878a0c4e6
Kevin Escobar Rodriguez <kevin.rodriguez@neuralmagic.com>
2021-08-24 16:15:48 -0400

    Added support for nightly versions for framework package version

--- src/sparseml/pytorch/framework/info.py
+++ src/sparseml/pytorch/framework/info.py
@@ -130,8 +130,16 @@ def framework_info() -> FrameworkInfo:
                 get_version(package_name="torchvision", raise_on_error=False)
             ),
             "onnx": get_version(package_name="onnx", raise_on_error=False),
-            "sparsezoo": get_version(package_name="sparsezoo", raise_on_error=False),
-            "sparseml": get_version(package_name="sparseml", raise_on_error=False),
+            "sparsezoo": get_version(
+                package_name="sparsezoo",
+                raise_on_error=False,
+                alternate_package_names=["sparsezoo-nightly"],
+            ),
+            "sparseml": get_version(
+                package_name="sparseml",
+                raise_on_error=False,
+                alternate_package_names=["sparseml-nightly"],
+            ),
         },
         sparsification=sparsification_info(),
         inference_providers=[cpu_provider, gpu_provider],
