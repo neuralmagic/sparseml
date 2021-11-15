8d98a8d01ee5ca321cd2044d6f3b5dc00421dca2
Mark Kurtz <mark.kurtz@neuralmagic.com>
2021-11-15 14:33:13 -0500

    update to allow nightly to be passed for dev builds

--- src/sparseml/__init__.py
+++ src/sparseml/__init__.py
@@ -44,8 +44,10 @@ from .sparsification import (
     load_sparsification_info,
 )
 
+
 from sparsezoo.package import check_package_version as _check_package_version
 
 _check_package_version(
-    package_name=__name__, package_integration=None, package_version=version_base
+    package_name=__name__ if is_release else f"{__name__}-nightly",
+    package_version=version_base,
 )
