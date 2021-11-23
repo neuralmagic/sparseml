bd189fcbc12d81a9dd7a2c05030bb9e0dab13922
Benjamin <ben@neuralmagic.com>
2021-11-23 16:38:15 -0500

    save torch recipes with state dict and apply on load

--- src/sparseml/pytorch/utils/model.py
+++ src/sparseml/pytorch/utils/model.py
@@ -85,6 +85,9 @@ def load_model(
     model_dict = torch.load(path, map_location="cpu")
     current_dict = model.state_dict()
 
+    checkpoint_epoch = model_dict.get("epoch", float("inf"))
+    checkpoint_recipe = model_dict.get("recipe", None)
+
     if "state_dict" in model_dict:
         model_dict = model_dict["state_dict"]
 
@@ -116,6 +119,15 @@ def load_model(
             model_dict[ignore] = current_dict[ignore]
         elif ignore in model_dict and ignore not in current_dict:
             del model_dict[ignore]
+
+    # apply checkpoint recipe if present to make any potential structural changes
+    if checkpoint_recipe is not None:
+        # import here to avoid cyclic dependencies
+        from sparseml.pytorch.optim import ScheduledModifierManager
+
+        ScheduledModifierManager.from_yaml(checkpoint_recipe).apply(
+            model, epoch=checkpoint_epoch
+        )
 
     model.load_state_dict(model_dict, strict)
 
