f448732050a991821fa052ab2b4895ae73f7ece1
Benjamin <ben@neuralmagic.com>
2022-01-24 19:25:50 -0500

    update transformers install to point to tarball + small fixes

--- src/sparseml/transformers/__init__.py
+++ src/sparseml/transformers/__init__.py
@@ -24,12 +24,19 @@ import logging as _logging
 try:
     import transformers as _transformers
 
+    # triggers error if neuralmagic/transformers is not installed
+    _transformers.models.bert.modeling_bert.QATMatMul
     _transformers_import_error = None
 except Exception as _transformers_import_err:
     _transformers_import_error = _transformers_import_err
 
 
 _LOGGER = _logging.getLogger(__name__)
+_NM_TRANSFORMERS_TAR_TEMPLATE = (
+    "https://github.com/neuralmagic/transformers/releases/download/"
+    "{version}/transformers-4.7.0.dev0.tar.gz"
+)
+_NM_TRANSFORMERS_NIGHTLY = _NM_TRANSFORMERS_TAR_TEMPLATE.format(version="nightly")
 
 
 def _install_transformers_and_deps():
@@ -37,16 +44,14 @@ def _install_transformers_and_deps():
     import pip as _pip
     import sparseml as _sparseml
 
-    transformers_branch = (
-        "master"
+    nm_transformers_release = (
+        "nightly"
         if not _sparseml.is_release
-        else f"release/{_sparseml.version_major_minor}"
+        else f"release/v{_sparseml.version_major_minor}.0"
     )
-    transformers_requirement = (
-        "transformers @ git+https://github.com/neuralmagic/transformers.git"
-        f"@{transformers_branch}"
+    transformers_requirement = _NM_TRANSFORMERS_TAR_TEMPLATE.format(
+        version=nm_transformers_release
     )
-
     try:
         _pip.main(
             [
@@ -65,7 +70,7 @@ def _install_transformers_and_deps():
         raise ValueError(
             "Unable to install and import sparseml-transformers dependencies check "
             "that transformers is installed, if not, install via "
-            "`pip install git+https://github.com/neuralmagic/transformers.git`"
+            f"`pip install {_NM_TRANSFORMERS_NIGHTLY}`"
         )
 
 
@@ -75,19 +80,21 @@ def _check_transformers_install():
 
         if os.getenv("NM_NO_AUTOINSTALL_TRANSFORMERS", False):
             _LOGGER.warning(
-                "Unable to import transformers, skipping auto installation "
+                "Unable to import, skipping auto installation "
                 "due to NM_NO_AUTOINSTALL_TRANSFORMERS"
             )
             # skip any further checks
             return
         else:
-            _LOGGER.info(
-                "No installation of transformers found. Installing sparseml-transformers "
-                "dependencies"
+            _LOGGER.warning(
+                "sparseml-transformers installation not detected. Installing "
+                "sparseml-transformers dependencies if transformers is already "
+                "installed in the environment, it will be overwritten. Set "
+                "environment variable NM_NO_AUTOINSTALL_TRANSFORMERS to disable"
             )
             _install_transformers_and_deps()
 
-    # check NM fork installed with QATMatMul available
+    # re check import after potential install
     try:
         import transformers as _transformers
 
@@ -97,7 +104,7 @@ def _check_transformers_install():
             "transformers.models.bert.modeling_bert.QATMatMul not availalbe. the"
             "neuralmagic fork of transformers may not be installed. it can be "
             "installed via "
-            "`pip install git+https://github.com/neuralmagic/transformers.git`"
+            f"`pip install {_NM_TRANSFORMERS_NIGHTLY}`"
         )
 
 
