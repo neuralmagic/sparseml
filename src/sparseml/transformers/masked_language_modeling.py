d55c6871175d07657c5cd69079fe595fd6a551fa
Alexandre Marques <alexandre@neuralmagic.com>
2022-02-18 17:54:34 -0500

    Added support to total batch size argument to masked lamguage modeling, question_answering and text classification

--- src/sparseml/transformers/masked_language_modeling.py
+++ src/sparseml/transformers/masked_language_modeling.py
@@ -45,12 +45,12 @@ from transformers import (
     AutoTokenizer,
     DataCollatorForLanguageModeling,
     HfArgumentParser,
-    TrainingArguments,
     set_seed,
 )
 from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version
 
+from sparseml.transformers.nm_training_arguments import NMTrainingArguments
 from sparseml.transformers.sparsification import Trainer
 from sparseml.transformers.utils import SparseAutoModel
 
@@ -272,7 +272,7 @@ def main():
     # We now keep distinct sets of args, for a cleaner separation of concerns.
 
     parser = HfArgumentParser(
-        (ModelArguments, DataTrainingArguments, TrainingArguments)
+        (ModelArguments, DataTrainingArguments, NMTrainingArguments)
     )
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         # If we pass only one argument to the script and it's the path to a json file,
