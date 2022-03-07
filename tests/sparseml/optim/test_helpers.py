b4189fa8b208663014b1b5981725dfec9d3d898c
dbogunowicz <97082108+dbogunowicz@users.noreply.github.com>
2022-03-07 12:46:24 +0100

    Ability to load recipe stages from YAML into dictionary of name -> List[modifiers] (#556)
    * first proposal of the solution
    
    * add docstring
    
    * stages support for Manager lifecycle
    
    * load staged recipe yaml string into list of dicts
    
    * modifiers properly read sample staged recipe
    
    * modifiers properly read sample staged recipe
    
    * edit after Ben's comments
    
    * tiny docstring fix
    
    * Removed stage_recipy.md from repository
    
    * Add fixes according to Ben's commments
    
    * Nit: remove print functions
    
    * Give priority to local variables over global when conflicting
    
    * Allow information from main_container flow to the evaluation of stage_containers
    
    Co-authored-by: bogunowicz@arrival.com <bogunowicz@arrival.com>
    Co-authored-by: Benjamin <ben@neuralmagic.com>

--- tests/sparseml/optim/test_helpers.py
+++ tests/sparseml/optim/test_helpers.py
@@ -15,12 +15,214 @@
 import pytest
 
 from sparseml.optim import (
+    check_if_staged_recipe,
     evaluate_recipe_yaml_str_equations,
     load_recipe_yaml_str,
     load_recipe_yaml_str_no_classes,
     update_recipe_variables,
 )
 
+
+STAGED_RECIPE_COMPLEX = """
+sparsity: 0.9
+init_lr: 0.05
+final_lr: 0.0
+end_epoch: 100
+update_frequency: 10
+start_epoch: 0.0
+global_sparsity: True
+
+ac_dc_phase:
+  update_frequency: 5
+  end_epoch_global: eval(end_epoch)
+  end_epoch: 75
+  num_epochs: 10
+  end_warm_up_epoch: 5.0
+  final_lr: eval(final_lr)
+  final_lr: 0.256
+  sparsity: 0.5
+  global_sparsity: False
+
+  training_modifiers:
+  - !EpochRangeModifier
+    start_epoch: eval(start_epoch)
+    end_epoch: eval(end_epoch_global)
+
+  - !LearningRateFunctionModifier
+    start_epoch: eval(start_epoch)
+    end_epoch: eval(end_warm_up_epoch)
+    lr_func: linear
+    init_lr: eval(init_lr)
+    final_lr: eval(final_lr)
+
+  - !LearningRateFunctionModifier
+    start_epoch: eval(end_warm_up_epoch)
+    end_epoch: eval(end_epoch_global)
+    lr_func: cosine
+    init_lr: eval(final_lr)
+    final_lr: 0.0
+
+  pruning_modifiers:
+
+  - !ACDCPruningModifier
+     compression_sparsity: eval(sparsity)
+     start_epoch: 1.0
+     end_epoch: eval(end_epoch)
+     update_frequency: eval(update_frequency)
+     params: ['re:.*conv*', 're:.*fc.weight*']
+     global_sparsity: eval(global_sparsity)
+
+next_stage:
+  new_num_epochs: 15
+
+  modifiers:
+    - !EpochRangeModifier
+        end_epoch: eval(end_epoch)
+        start_epoch: eval(start_epoch + new_num_epochs)
+
+    - !GMPruningModifier
+        end_epoch: eval(end_epoch)
+        final_sparsity: eval(sparsity)
+        init_sparsity: eval(sparsity)
+"""
+
+STAGED_RECIPE_COMPLEX_EVAL = """
+sparsity: 0.9
+init_lr: 0.05
+final_lr: 0.0
+end_epoch: 100
+update_frequency: 10
+start_epoch: 0.0
+global_sparsity: True
+
+ac_dc_phase:
+  update_frequency: 5
+  end_epoch_global: 100
+  end_epoch: 75
+  num_epochs: 10
+  end_warm_up_epoch: 5.0
+  final_lr: 0.256
+  sparsity: 0.5
+  global_sparsity: False
+
+  training_modifiers:
+  - !EpochRangeModifier
+    start_epoch: 0.0
+    end_epoch: 100
+
+  - !LearningRateFunctionModifier
+    start_epoch: 0.0
+    end_epoch: 5.0
+    lr_func: linear
+    init_lr: 0.05
+    final_lr: 0.256
+
+  - !LearningRateFunctionModifier
+    start_epoch: 5.0
+    end_epoch: 100
+    lr_func: cosine
+    init_lr: 0.256
+    final_lr: 0.0
+
+  pruning_modifiers:
+
+  - !ACDCPruningModifier
+     compression_sparsity: 0.5
+     start_epoch: 1.0
+     end_epoch: 75
+     update_frequency: 5
+     params: ['re:.*conv*', 're:.*fc.weight*']
+     global_sparsity: False
+
+next_stage:
+  new_num_epochs: 15
+
+  modifiers:
+    - !EpochRangeModifier
+        end_epoch: 100
+        start_epoch: 15.0
+
+    - !GMPruningModifier
+        end_epoch: 100
+        final_sparsity: 0.9
+        init_sparsity: 0.9
+"""
+
+STAGED_RECIPE_SIMPLE = """
+first_variable: 10
+second_variable: 5
+lr_multiplier: 2
+
+first_stage:
+  lr: 0.1
+  num_epochs: 10
+  init_lr: eval(lr * 2)
+  final_lr: eval(lr)
+
+  training_modifiers:
+    - !EpochRangeModifier
+        end_epoch: eval(num_epochs + first_variable)
+        start_epoch: 0.0
+
+    - !LearningRateFunctionModifier
+      start_epoch: 0
+      end_epoch: eval(num_epochs)
+      lr_func: linear
+      init_lr: eval(init_lr)
+      final_lr: eval(final_lr)
+
+next_stage:
+  new_num_epochs: 15
+  sparsity: 0.9
+
+  modifiers:
+    - !EpochRangeModifier
+        end_epoch: eval(new_num_epochs)
+        start_epoch: eval(second_variable)
+
+    - !GMPruningModifier
+        end_epoch: eval(second_variable + first_variable)
+        final_sparsity: eval(sparsity)
+        init_sparsity: eval(sparsity)
+"""
+
+STAGED_RECIPE_SIMPLE_EVAL = """
+first_variable: 10
+second_variable: 5
+lr_multiplier: 2
+
+first_stage:
+  lr: 0.1
+  num_epochs: 10
+  init_lr: 0.2
+  final_lr: 0.1
+
+  training_modifiers:
+    - !EpochRangeModifier
+        end_epoch: 20
+        start_epoch: 0.0
+
+    - !LearningRateFunctionModifier
+      start_epoch: 0
+      end_epoch: 10
+      lr_func: linear
+      init_lr: 0.2
+      final_lr: 0.1
+
+next_stage:
+  new_num_epochs: 15
+  sparsity: 0.9
+
+  modifiers:
+    - !EpochRangeModifier
+        end_epoch: 15
+        start_epoch: 5
+
+    - !GMPruningModifier
+        end_epoch: 15
+        final_sparsity: 0.9
+        init_sparsity: 0.9
+"""
 
 RECIPE_SIMPLE_EVAL = """
 num_epochs: 10.0
@@ -46,7 +248,6 @@ modifiers:
         start_epoch: eval(pruning_start_epoch)
         update_frequency: 0.01
 """
-
 
 RECIPE_MULTI_EVAL = """
 num_epochs: 10.0
@@ -117,18 +318,25 @@ def _test_nested_equality(val, other):
 
 
 @pytest.mark.parametrize(
-    "recipe,expected_recipe",
+    "recipe,expected_recipe, is_staged",
     [
-        (TARGET_RECIPE.format(num_epochs=10.0), TARGET_RECIPE.format(num_epochs=10.0)),
-        (RECIPE_SIMPLE_EVAL, TARGET_RECIPE.format(num_epochs=10.0)),
-        (RECIPE_MULTI_EVAL, TARGET_RECIPE.format(num_epochs=10.0)),
+        (
+            TARGET_RECIPE.format(num_epochs=10.0),
+            TARGET_RECIPE.format(num_epochs=10.0),
+            False,
+        ),
+        (RECIPE_SIMPLE_EVAL, TARGET_RECIPE.format(num_epochs=10.0), False),
+        (RECIPE_MULTI_EVAL, TARGET_RECIPE.format(num_epochs=10.0), False),
+        (STAGED_RECIPE_SIMPLE, STAGED_RECIPE_SIMPLE_EVAL, True),
+        (STAGED_RECIPE_COMPLEX, STAGED_RECIPE_COMPLEX_EVAL, True),
     ],
 )
-def test_evaluate_recipe_yaml_str_equations(recipe, expected_recipe):
+def test_evaluate_recipe_yaml_str_equations(recipe, expected_recipe, is_staged):
     evaluated_recipe = evaluate_recipe_yaml_str_equations(recipe)
     evaluated_yaml = load_recipe_yaml_str_no_classes(evaluated_recipe)
+    expected_is_staged = check_if_staged_recipe(evaluated_yaml)
     expected_yaml = load_recipe_yaml_str_no_classes(expected_recipe)
-
+    assert expected_is_staged == is_staged
     assert isinstance(evaluated_yaml, dict)
     assert isinstance(expected_yaml, dict)
     _test_nested_equality(evaluated_yaml, expected_yaml)
