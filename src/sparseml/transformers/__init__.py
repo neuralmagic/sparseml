c14314a07dc07c5da57b2b56d33ae11a51037b6c
Benjamin <ben@neuralmagic.com>
2022-03-17 14:33:30 -0400

    update transformers url for rebase version

--- src/sparseml/transformers/__init__.py
+++ src/sparseml/transformers/__init__.py
@@ -32,11 +32,22 @@ except Exception as _transformers_import_err:
 
 
 _LOGGER = _logging.getLogger(__name__)
-_NM_TRANSFORMERS_TAR_TEMPLATE = (
-    "https://github.com/neuralmagic/transformers/releases/download/"
-    "{version}/transformers-4.7.0.dev0-py3-none-any.whl"
-)
-_NM_TRANSFORMERS_NIGHTLY = _NM_TRANSFORMERS_TAR_TEMPLATE.format(version="nightly")
+
+
+def _get_transformers_install_url(version):
+    if version == "nightly":
+        transformers_base_version = "4.18.0"
+        release = "nightly"
+    else:
+        transformers_base_version = "4.18.0" if version >= "0.12" else "4.7.0"
+        release = f"v{version}"
+    return (
+        "https://github.com/neuralmagic/transformers/releases/download/"
+        f"{release}/transformers-{transformers_base_version}.dev0-py3-none-any.whl"
+    )
+
+
+_NM_TRANSFORMERS_NIGHTLY = _get_transformers_install_url("nightly")
 
 
 def _install_transformers_and_deps():
@@ -46,12 +57,10 @@ def _install_transformers_and_deps():
 
     import sparseml as _sparseml
 
-    nm_transformers_release = (
-        "nightly" if not _sparseml.is_release else f"v{_sparseml.version_major_minor}"
+    transformers_requirement = _get_transformers_install_url(
+        "nightly" if not _sparseml.is_release else _sparseml.version_major_minor
     )
-    transformers_requirement = _NM_TRANSFORMERS_TAR_TEMPLATE.format(
-        version=nm_transformers_release
-    )
+
     try:
         _subprocess.check_call(
             [
