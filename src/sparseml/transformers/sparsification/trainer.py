471926e70ebfca33c16d11447182bc2feb2958c1
Konstantin Gulin <66528950+KSGulin@users.noreply.github.com>
2022-03-21 13:43:31 +0000

    [Transformers] Initialize with grad_sampler when no scaler (#630)
    * Fix: grad scaler correct passing when no scaler

--- src/sparseml/transformers/sparsification/trainer.py
+++ src/sparseml/transformers/sparsification/trainer.py
@@ -251,13 +251,14 @@ class RecipeManagerTrainerInterface:
                 self.manager,
                 steps_per_epoch=self.manager_steps_per_epoch,
                 loggers=self.logger_manager,
-                grad_sampler=self.grad_sampler,
+                initialize_kwargs={"grad_sampler": self.grad_sampler},
             )
             if not self.manager.initialized:
                 self.manager.initialize(
                     self.model,
                     loggers=self.logger_manager,
                     distillation_teacher=self.teacher,
+                    grad_sampler=self.grad_sampler,
                 )
         self.manager_initialized = True
         _LOGGER.info(
