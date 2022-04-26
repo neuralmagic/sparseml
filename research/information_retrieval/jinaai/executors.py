534a898cf05ddd3b9f1aaa60f346417d594cd085
bogunowicz@arrival.com <bogunowicz@arrival.com>
2022-04-26 11:58:23 +0200

    Finish JinaAI integration

--- research/information_retrieval/jinaai/executors.py
+++ research/information_retrieval/jinaai/executors.py
@@ -1,9 +1,21 @@
-from deepsparse import pipeline
+"""
+Executor is a self-contained component and performs a group of tasks on a DocumentArray.
+This is the component, which in our integration should wrap around DeepSpare pipeline.
+
+Source: https://docs.jina.ai/fundamentals/executor/
+"""
+
+from deepsparse.transformers import pipeline
 from docarray import Document, DocumentArray
 from jina import Executor, requests
 
 
 class DeepSparseExecutor(Executor):
+    """
+    Minimal integration requires a
+        - constructor
+        - at least one function decorated by @request (endpoint)
+    """
     def __init__(self, model_stub, **kwargs):
         self.pipeline = pipeline(
             task="question-answering",
@@ -13,9 +25,14 @@ class DeepSparseExecutor(Executor):
 
     @requests(on='/question_answering')
     def run(self, docs, **kwargs):
-        pass
         question = docs[0].content
         context = docs[1].content
 
-        answer = self.pipeline(question=question, content=context)
-        return DocumentArray(Document(text=question))
+        answer = self.pipeline(question=question, context=context)
+        """
+        Endpoint function returns one of three:
+            - if return None, will return (potentially mutated) docs variable
+            - DocumentArray
+            - dict (will be passed along with the potentially mutated docs variable)
+            """
+        return DocumentArray(Document(text=answer['answer']))
--- research/information_retrieval/jinaai/minimal_example.py
+++ research/information_retrieval/jinaai/minimal_example.py
@@ -1,25 +1,34 @@
+"""
+To run:
+1. pip install deepsparse
+2. pip install -U jina
+3. python minimal_example.py
+"""
+
 from docarray import Document, DocumentArray
 from jina import Flow
-
 from executors import DeepSparseExecutor
+import time
 
-# Create a document with a query and context
 query, context = "What's my name?", "My name is Mario!"
+# JinaAI uses DocumentArray as its native data structure
 docs = DocumentArray([Document(text=query),
                       Document(text=context)
                       ])
+
 # Pass the SparseZoo model
 model_stub = "zoo:nlp/question_answering/bert-base/pytorch/huggingface/squad/pruned_3layers-aggressive_90"
 
-# Create a new Flow that includes our custom Executor
+# Create a new Flow that includes our custom DeepSparseExecutor
 flow = (Flow().add(uses=DeepSparseExecutor,
                    uses_with={'model_stub': model_stub},
-                   name="executor",
-                   install_requirements=True))
+                   name="deepsparse_executor"))
 
 # Open Flow as context manager
+time1 = time.time()
 with flow:
     answer = flow.post(on='/question_answering', inputs=docs)
+time_delta = time.time() - time1
 
 print(f"Input question: {query}, input context: {context}")
-print(f"Response: {answer.contents[0]}")
+print(f"Response: {answer.contents[0]} (took {time_delta}s)")
