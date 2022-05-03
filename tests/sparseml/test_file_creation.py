4d0deff5addaedfa11d99e3079349a4e200046c2
Konstantin <konstantin@neuralmagic.com>
2022-05-03 17:57:09 -0400

    Update: yolov5 initial tests

--- tests/sparseml/test_file_creation.py
+++ tests/sparseml/test_file_creation.py
@@ -11,26 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import pytest
-
-from tests.integrations.base_tester import BaseIntegrationTester
-from yolov5.export import create_checkpoint, load_checkpoint
-from yolov5.val import run as val
-
-from .yolov5_args import Yolov5TrainArgs
-
-
-@pytest.mark.usefixtures("setup")
-class Yolov5IntegrationTester(BaseIntegrationTester):
-    command_stubs = {
-        "train": "sparseml.yolov5.train",
-        "export": "sparseml.yolov5.export",
-        "deploy": "sparseml.yolov5.deploy",
-    }
-    command_args_classes = {
-        "train": Yolov5TrainArgs,
-    }
-
-    def test_checkpoint_load(self, setup):
-        pass
