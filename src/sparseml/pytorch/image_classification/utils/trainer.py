a7ef7198f5bd4b96ebb1c21d5fdf6c9e7f4e4a79
Rahul Tuli <rahul@neuralmagic.com>
2022-05-10 14:39:01 -0400

    Add: `max-eval-steps` arg Remove: `debug-steps` arg Update: Docstrings Add: example command and update docstring Add: Max Train Steps Support

--- src/sparseml/pytorch/image_classification/utils/trainer.py
+++ src/sparseml/pytorch/image_classification/utils/trainer.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 """
-Trainers for image classification.
+Trainers for image classification
 """
 
 import logging
@@ -44,45 +44,48 @@ __all__ = [
 
 class Trainer(ABC):
     """
-    Abstract class for Trainers.
-    Creates a contract that all trainers must have a run_one_epoch method.
+    Abstract class for Trainers
+    Creates a contract that all trainers must have a run_one_epoch method
     """
 
     @abstractmethod
     def run_one_epoch(self):
         """
-        Runs one epoch of training.
+        Runs one epoch of training
         """
         raise NotImplementedError
 
 
 class ImageClassificationTrainer(Trainer):
     """
-    Trainer for image classification.
+    Trainer for image classification
 
-    :param model: The loaded torch model to train.
-    :param key: The arch key of the model.
+    :param model: The loaded torch model to train
+    :param key: The arch key of the model
     :param recipe_path: The path to the yaml file containing the modifiers and
         schedule to apply them with; Can also provide a SparseZoo stub prefixed
-        with 'zoo:'.
-    :param ddp: bool indicating whether to use Distributed Data Parallel.
-    :param device: The device to train on. Defaults to torch default device.
-    :param use_mixed_precision: Whether to use mixed precision FP16 training.
-        Defaults to False.
-    :param val_loader: A DataLoader for validation data.
-    :param train_loader: A DataLoader for training data.
+        with 'zoo:'
+    :param ddp: bool indicating whether to use Distributed Data Parallel
+    :param device: The device to train on Defaults to torch default device
+    :param use_mixed_precision: Whether to use mixed precision FP16 training
+        Defaults to False
+    :param val_loader: A DataLoader for validation data
+    :param train_loader: A DataLoader for training data
     :param is_main_process: Whether the current process is the main process,
-        while training using DDP. Defaults to True.
-    :param loggers: A list of loggers to use during training process.
+        while training using DDP. Defaults to True
+    :param loggers: A list of loggers to use during training process
     :param loss_fn: A Callable loss function for training and validation
-        losses.
+        losses
     :param init_lr: The initial learning rate for the optimizer.Defaults to
-        1e-9.
+        1e-9
     :param optim_name: str representing the optimizer type to use.
-        Defaults to `Adam`.
-    :param optim_kwargs: dict of additional kwargs to pass to the optimizer.
+        Defaults to `Adam`
+    :param optim_kwargs: dict of additional kwargs to pass to the optimizer
     :param recipe_args: json parsable dict of recipe variable names to values
+        to overwrite with
+    :param max_train_steps: The maximum number of training steps to run per epoch
         to overwrite with.
+    :param one_shot: bool indicating whether to apply recipe in one shot manner
     """
 
     def __init__(
@@ -104,9 +107,11 @@ class ImageClassificationTrainer(Trainer):
         optim_name="Adam",
         optim_kwargs: Optional[Dict[str, Any]] = None,
         recipe_args: Optional[str] = None,
+        max_train_steps: int = -1,
+        one_shot: bool = False,
     ):
         """
-        Initializes the module_trainer.
+        Initializes the module_trainer
         """
         self.recipe_path = recipe_path
         self.metadata = metadata
@@ -123,6 +128,8 @@ class ImageClassificationTrainer(Trainer):
         self.train_loader = train_loader
         self.loggers = loggers
         self.recipe_args = recipe_args
+        self.max_train_steps = max_train_steps
+        self.one_shot = one_shot
 
         self.val_loss = loss_fn()
         _LOGGER.info(f"created loss for validation: {self.val_loss}")
@@ -135,7 +142,7 @@ class ImageClassificationTrainer(Trainer):
         self._device_context = ModuleDeviceContext(
             use_mixed_precision=self.use_mixed_precision,
         )
-        if self.train_loader is not None:
+        if self.train_loader is not None and not self.one_shot:
             (
                 self.epoch,
                 self.optim,
@@ -144,6 +151,8 @@ class ImageClassificationTrainer(Trainer):
             self.module_trainer = self._initialize_module_trainer()
         else:
             self.optim = self.manager = self.module_trainer = None
+            if self.one_shot:
+                self._apply_one_shot()
 
         self.checkpoint_manager = (
             self._setup_checkpoint_manager() if self.checkpoint_path else None
@@ -172,7 +181,7 @@ class ImageClassificationTrainer(Trainer):
         baseline_run: bool = False,
     ) -> Any:
         """
-        Runs one epoch of training or validation.
+        Runs one epoch of training or validation
 
         :param mode: str representing the mode to run in, one of
             ['train', 'val']
@@ -204,6 +213,14 @@ class ImageClassificationTrainer(Trainer):
         """
         return self.manager.max_epochs if self.manager is not None else 0
 
+    def _apply_one_shot(self):
+        self.manager = ScheduledModifierManager.from_yaml(
+            self.recipe_path,
+        )
+
+        self.manager.apply(self.model)
+        _LOGGER.info(f"Applied {self.recipe_path} to manager")
+
     def _initialize_module_tester(self):
         tester = ModuleTester(
             module=self.model,
@@ -234,11 +251,14 @@ class ImageClassificationTrainer(Trainer):
             metadata=self.metadata,
         )
 
+        steps_per_epoch = (
+            len(self.train_loader) if self.max_train_steps < 0 else self.max_train_steps
+        )
         optim = ScheduledOptimizer(
             optim,
             self.model.module if is_parallel_model(self.model) else self.model,
             manager,
-            steps_per_epoch=len(self.train_loader),
+            steps_per_epoch=steps_per_epoch,
             loggers=self.loggers,
         )
         _LOGGER.info(f"created manager: {manager}")
