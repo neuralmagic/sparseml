43c1be85901d145bf510b3121b37fcc53de245eb
Konstantin <konstantin@neuralmagic.com>
2022-05-11 13:53:10 -0400

    Update export testing

--- tests/integrations/yolov5/test_yolov5_integration.py
+++ tests/integrations/yolov5/test_yolov5_integration.py
@@ -14,15 +14,18 @@
 
 import os
 import tempfile
+from collections import Counter
 
 import onnx
 import pytest
+import torch
 
 from tests.integrations.base_tester import BaseIntegrationTester
 from tests.integrations.helpers import get_configs_with_cadence, skip_inactive_stage
 from tests.integrations.yolov5.yolov5_args import Yolov5ExportArgs, Yolov5TrainArgs
 from yolov5.utils.general import ROOT
 from yolov5.val import run as val
+from yolov5.export import load_checkpoint
 
 
 METRIC_TO_INDEX = {"map0.5": 2}
@@ -52,8 +55,7 @@ class TestYolov5Integration(BaseIntegrationTester):
             )
 
     def teardown(self):
-        # self.save_dir.cleanup()
-        pass
+        self.save_dir.cleanup()
 
 
 @pytest.fixture(
@@ -69,15 +71,25 @@ def yolov5_tester(request):
 
 
 @skip_inactive_stage
-def test_train_val(yolov5_tester):
+def test_train_complete(yolov5_tester):
     tester = yolov5_tester
+    train_args = tester.configs["train"]["args"]
     model_file = os.path.join(tester.save_dir.name, "exp", "weights", "last.pt")
     assert os.path.isfile(model_file)
-    metrics, *_ = val(data=ROOT / "data/coco128.yaml", weights=model_file)
+    #model, extras = load_checkpoint(type_="val",weights = model_file)
+    #assert extras["ckpt"]["epoch"] == 
+
+
+@skip_inactive_stage
+def test_train_metrics(yolov5_tester):
+    tester = yolov5_tester
+    train_args = tester.configs["train"]["args"]
+    model_file = os.path.join(tester.save_dir.name, "exp", "weights", "last.pt")    
+    metrics, *_ = val(data=train_args.data, weights=model_file)
     if "target_name" in tester.test_args["train"]:
         test_args = tester.test_args["train"]
         metric_idx = METRIC_TO_INDEX[test_args["target_name"]]
-        metric = metrics[metric_idx]
+        metric = metrics[metric_idx]*100
         target_mean = test_args["target_mean"]
         target_std = test_args["target_std"]
         assert target_mean - target_std <= metric <= target_mean + target_std
@@ -86,8 +98,41 @@ def test_train_val(yolov5_tester):
 @skip_inactive_stage
 def test_export_onnx_graph(yolov5_tester):
     tester = yolov5_tester
+    train_args = tester.configs["train"]["args"]
+    test_args = tester.test_args["export"]
     onnx_file = os.path.join(
         os.path.dirname(tester.configs["export"]["args"].weights), "last.onnx"
     )
     assert os.path.isfile(onnx_file)
     model = onnx.load(onnx_file)
+    onnx.checker.check_model(model)
+    target_model_path = test_args.get("target_model")
+    if target_model_path:
+        target_model, extras = load_checkpoint(type_="val",weights = target_model_path, cfg = train_args.cfg, device = torch.device("cpu"))
+        _compare_onnx_models(model, target_model)
+        
+
+
+def _compare_onnx_models(model_1, model_2):
+            major_nodes = [
+                "QLinearMatMul",
+                "Gemm",
+                "MatMul",
+                "MatMulInteger",
+                "Conv",
+                "QLinearConv",
+                "ConvInteger",
+                "QuantizeLinear",
+                "DeQuantizeLinear",
+            ]
+
+            nodes1 = model_1.graph.node
+            nodes1_names = [node.name for node in nodes1]
+            nodes1_count = Counter([node_name.split("_")[0] for node_name in nodes1_names])
+
+            nodes2 = model_2.graph.node
+            nodes2_names = [node.name for node in nodes2]
+            nodes2_count = Counter([node_name.split("_")[0] for node_name in nodes2_names])
+
+            for node in major_nodes:
+                assert nodes1_count[node] == nodes2_count[node]
