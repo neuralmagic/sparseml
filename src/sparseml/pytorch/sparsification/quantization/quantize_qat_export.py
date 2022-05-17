b68abda7909674f416c60950f0e9bedf854d90f0
Alexandre Marques <alexandre@neuralmagic.com>
2022-05-17 14:17:44 -0400

    Remove unnecessary call

--- src/sparseml/pytorch/sparsification/quantization/quantize_qat_export.py
+++ src/sparseml/pytorch/sparsification/quantization/quantize_qat_export.py
@@ -1548,7 +1548,6 @@ def quantize_torch_qat_export(
     _fold_relu_quants(model)
     _convert_single_constants_to_initializers(model)
     _delete_repeated_qat_blocks(model)
-    # _convert_quantizable_matmul_matmul(model)
     _convert_quantizable_matmul(model)
     _convert_quantizable_matmul_and_add(model)
 
