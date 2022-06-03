21d24924fcdfa125c187ad3a93ed17c9c688d10e
Tuan Nguyen <tuan@neuralmagic.com>
2022-06-03 10:11:12 -0400

    Format training args

--- src/sparseml/transformers/sparsification/training_args.py
+++ src/sparseml/transformers/sparsification/training_args.py
@@ -1,6 +1,21 @@
+# Copyright (c) 2022 - present / Neuralmagic, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from transformers import TrainingArguments as HFTrainingArgs
 
 __all__ = ["TrainingArguments"]
+
 
 @dataclass
 class TrainingArguments(HFTrainingArgs):
@@ -11,8 +26,8 @@ class TrainingArguments(HFTrainingArgs):
         The epoch after which best model will be saved; used in conjunction with `load_best_model_at_end` and
         `metric_for_best_model` training arguments
     """
+
     best_model_after_epoch: int = field(
         default=None,
         metadata={"help": "Epoch after which best model will be saved."},
     )
-
