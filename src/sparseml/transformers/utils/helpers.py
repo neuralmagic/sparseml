8ac13bc901517e394f47419f3b7a8d58fafd4721
bogunowicz@arrival.com <bogunowicz@arrival.com>
2022-07-01 20:35:17 +0200

    initial commit

--- src/sparseml/transformers/utils/helpers.py
+++ src/sparseml/transformers/utils/helpers.py
@@ -16,10 +16,91 @@
 Helper variables and functions for integrating SparseML with huggingface/transformers
 flows
 """
+import logging
+import os
+from typing import Optional
 
-__all__ = [
-    "RECIPE_NAME",
-]
+from sparsezoo.v2.helpers import setup_model_directory
+
+
+__all__ = ["RECIPE_NAME", "get_model_directory"]
 
 
 RECIPE_NAME = "recipe.yaml"
+TRAINING_FILES = [
+    "eval_results.json",
+    "training_args.bin",
+    "vocab.txt",
+    "all_results.json",
+    "trainer_state.json",
+    "special_tokens_map.json",
+    "train_results.json",
+    "recipe.yaml",
+    "pytorch_model.bin",
+    "config.json",
+    "tokenizer_config.json",
+    "tokenizer.json",
+]
+DEPLOYMENT_FILES = ["tokenizer_config.json", "config.json", "model.onnx"]
+
+
+def get_model_directory(
+    output_dir: str,
+    training_outputs_dir: str,
+    logs_path: Optional[str] = None,
+    training_files: str = TRAINING_FILES,
+    deployment_files: str = DEPLOYMENT_FILES,
+) -> None:
+    """
+    Takes the `training_outputs_dir`
+    (the directory where the pipeline saves its training artifacts),
+    and saves the training artifacts to `output_dir` in the `ModelDirectory` structure.
+
+    :param output_dir: The output path where the artifacts are saved
+        (adhering to the in `ModelDirectory` structure)
+    :param training_outputs_dir: The path to the existing directory
+        with the saved training artifacts
+    :param logs_path: Optional directory where the training logs reside
+    :param training_files: List with files that belong to the `training` directory
+    :param deployment_files: List with files that belong to the `deployment` directory
+    """
+    for root_file in ["sample_inputs", "sample_outputs"]:
+        root_file_path = os.path.join(training_outputs_dir, root_file)
+        if not os.path.exists(root_file_path):
+            raise ValueError(
+                f"File {root_file_path} missing. To create this file, "
+                "make sure that the training script is being ran with"
+                "`--num_export_samples` argument."
+            )
+
+    model_onnx_path = os.path.join(training_outputs_dir, "model.onnx")
+    if not os.path.exists(model_onnx_path):
+        raise ValueError(
+            f"File {model_onnx_path} missing. To create this file, "
+            "make sure that the `export` script (for exporting "
+            "transformer models) has been evoked."
+        )
+
+    setup_model_directory(
+        output_dir=output_dir,
+        training=[
+            os.path.join(training_outputs_dir, file_name)
+            for file_name in training_files
+        ],
+        deployment=[
+            os.path.join(training_outputs_dir, file_name)
+            for file_name in deployment_files
+        ],
+        onnx_model=os.path.join(training_outputs_dir, "model.onnx"),
+        sample_inputs=os.path.join(training_outputs_dir, "sample_inputs"),
+        sample_outputs=os.path.join(training_outputs_dir, "sample_outputs"),
+        model_card=os.path.join(training_outputs_dir, "model.md"),
+        logs=logs_path,
+        sample_labels=None,
+        sample_originals=None,
+        analysis=None,
+        benchmarks=None,
+        eval_results=None,
+        recipes=None,
+    )
+    logging.info(f"Created `ModelDirectory` folder locally in {output_dir}")
