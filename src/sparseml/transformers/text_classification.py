771e3025613bd887d4495a262623add609a0bcc5
Eldar Kurtic <eldar.ciki@gmail.com>
2022-08-31 19:59:43 +0200

    Fix quality

--- src/sparseml/transformers/text_classification.py
+++ src/sparseml/transformers/text_classification.py
@@ -293,7 +293,8 @@ class ModelArguments:
 
 
 def main(**kwargs):
-    # See all possible arguments in src/sparseml/transformers/sparsification/training_args.py
+    # See all possible arguments in
+    # src/sparseml/transformers/sparsification/training_args.py
     # or by passing the --help flag to this script.
     # We now keep distinct sets of args, for a cleaner separation of concerns.
 
