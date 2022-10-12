c4c4e3491bcf73e08b6c784b5f258668663f8104
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-12 22:40:58 +0000

    Adding tarfile member sanitization to extractall()

--- src/sparseml/tensorflow_v1/datasets/classification/cifar.py
+++ src/sparseml/tensorflow_v1/datasets/classification/cifar.py
@@ -228,7 +228,26 @@ class Cifar10DataSet(CifarDataSet):
 
         create_dirs(self._extract_dir)
         with tarfile.open(file_path, "r:gz") as tar:
-            tar.extractall(path=self._extract_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=self._extract_dir)
 
     def _create_image_folders(self):
         create_dirs(self._train_dir)
@@ -325,7 +344,26 @@ class Cifar100DataSet(CifarDataSet):
 
         create_dirs(self._extract_dir)
         with tarfile.open(file_path, "r:gz") as tar:
-            tar.extractall(path=self._extract_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=self._extract_dir)
 
     def _create_image_folders(self):
         create_dirs(self._train_dir)
--- src/sparseml/utils/datasets/imagenette.py
+++ src/sparseml/utils/datasets/imagenette.py
@@ -167,7 +167,26 @@ class ImagenetteDownloader(object):
         )
 
         with tarfile.open(file_path, "r:gz") as tar:
-            tar.extractall(path=self.download_root)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=self.download_root)
 
 
 class ImagewoofDownloader(object):
@@ -276,4 +295,23 @@ class ImagewoofDownloader(object):
         )
 
         with tarfile.open(file_path, "r:gz") as tar:
-            tar.extractall(path=self.download_root)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=self.download_root)
