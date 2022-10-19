781e135dbedfcf794a7dc2dda61db2df555f8063
Rahul Tuli <rahul@neuralmagic.com>
2022-10-19 15:20:46 -0400

    Add a description to markdown files

--- src/sparseml/pytorch/recipe_template/main.py
+++ src/sparseml/pytorch/recipe_template/main.py
@@ -39,6 +39,37 @@ __all__ = [
     "recipe_template",
 ]
 
+# flake8: noqa W293, W291
+__DESC = """
+This recipe defines the hyperparams necessary to apply specified sparsification 
+techniques.
+ 
+Users are encouraged to experiment with the training length and initial learning 
+rate to either expedite training or to produce a more accurate model.
+
+This can be done by either editing the recipe or supplying the `--recipe_args` 
+argument to the training commands.
+
+For example, the following appended to the training commands will change the number 
+of epochs and the initial learning rate:
+
+```bash
+--recipe_args '{"num_epochs":8,"init_lr":0.0001}'
+```
+
+The following template can be used to apply this recipe in a `one-shot` manner:
+
+```python
+from sparseml.pytorch.optim import ScheduledModifierManager
+
+model = ... # instantiated model
+recipe = ... # local path to this recipe
+
+manager = ScheduledModifierManager.from_yaml(recipe)
+manager.apply(model) 
+```
+"""
+
 
 def recipe_template(
     pruning: str = "false",
@@ -165,9 +196,9 @@ def _build_recipe_template(
     return recipe
 
 
-def _add_description(recipe: str) -> str:
+def _add_description(recipe: str, description: str = __DESC) -> str:
     recipe = f"---\n{recipe}\n---"
-    return recipe
+    return "\n".join((recipe, description))
 
 
 def _write_recipe_to_file(file_name: str, recipe: str):
