56333a98ae841947f065046a2cab43fa9d925e25
Corey Lowman <corey@neuralmagic.com>
2022-10-24 17:36:10 +0000

    Removing lr schedule from torchvision train

--- src/sparseml/pytorch/torchvision/train.py
+++ src/sparseml/pytorch/torchvision/train.py
@@ -73,9 +73,6 @@ def train_one_epoch(
 
         if model_ema and i % args.model_ema_steps == 0:
             model_ema.update_parameters(model)
-            if epoch < args.lr_warmup_epochs:
-                # Reset ema buffer to keep copying weights during warmup period
-                model_ema.n_averaged.fill_(0)
 
         acc1, acc5 = utils.accuracy(output, target, topk=(1, 5))
         batch_size = image.shape[0]
@@ -335,50 +332,6 @@ def main(args):
 
     scaler = torch.cuda.amp.GradScaler() if args.amp else None
 
-    args.lr_scheduler = args.lr_scheduler.lower()
-    if args.lr_scheduler == "steplr":
-        main_lr_scheduler = torch.optim.lr_scheduler.StepLR(
-            optimizer, step_size=args.lr_step_size, gamma=args.lr_gamma
-        )
-    elif args.lr_scheduler == "cosineannealinglr":
-        main_lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-            optimizer, T_max=args.epochs - args.lr_warmup_epochs, eta_min=args.lr_min
-        )
-    elif args.lr_scheduler == "exponentiallr":
-        main_lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(
-            optimizer, gamma=args.lr_gamma
-        )
-    else:
-        raise RuntimeError(
-            f"Invalid lr scheduler '{args.lr_scheduler}'. Only StepLR, CosineAnnealingLR and ExponentialLR "
-            "are supported."
-        )
-
-    if args.lr_warmup_epochs > 0:
-        if args.lr_warmup_method == "linear":
-            warmup_lr_scheduler = torch.optim.lr_scheduler.LinearLR(
-                optimizer,
-                start_factor=args.lr_warmup_decay,
-                total_iters=args.lr_warmup_epochs,
-            )
-        elif args.lr_warmup_method == "constant":
-            warmup_lr_scheduler = torch.optim.lr_scheduler.ConstantLR(
-                optimizer,
-                factor=args.lr_warmup_decay,
-                total_iters=args.lr_warmup_epochs,
-            )
-        else:
-            raise RuntimeError(
-                f"Invalid warmup lr method '{args.lr_warmup_method}'. Only linear and constant are supported."
-            )
-        lr_scheduler = torch.optim.lr_scheduler.SequentialLR(
-            optimizer,
-            schedulers=[warmup_lr_scheduler, main_lr_scheduler],
-            milestones=[args.lr_warmup_epochs],
-        )
-    else:
-        lr_scheduler = main_lr_scheduler
-
     model_without_ddp = model
     if args.distributed:
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
@@ -404,7 +357,6 @@ def main(args):
         model_without_ddp.load_state_dict(checkpoint["model"])
         if not args.test_only:
             optimizer.load_state_dict(checkpoint["optimizer"])
-            lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
         args.start_epoch = checkpoint["epoch"] + 1
         if model_ema:
             model_ema.load_state_dict(checkpoint["model_ema"])
@@ -439,7 +391,6 @@ def main(args):
             model_ema,
             scaler,
         )
-        lr_scheduler.step()
         evaluate(model, criterion, data_loader_test, device=device)
         if model_ema:
             evaluate(
@@ -449,7 +400,6 @@ def main(args):
             checkpoint = {
                 "model": model_without_ddp.state_dict(),
                 "optimizer": optimizer.state_dict(),
-                "lr_scheduler": lr_scheduler.state_dict(),
                 "epoch": epoch,
                 "args": args,
             }
@@ -555,45 +505,6 @@ def get_args_parser(add_help=True):
     )
     parser.add_argument(
         "--cutmix-alpha", default=0.0, type=float, help="cutmix alpha (default: 0.0)"
-    )
-    parser.add_argument(
-        "--lr-scheduler",
-        default="steplr",
-        type=str,
-        help="the lr scheduler (default: steplr)",
-    )
-    parser.add_argument(
-        "--lr-warmup-epochs",
-        default=0,
-        type=int,
-        help="the number of epochs to warmup (default: 0)",
-    )
-    parser.add_argument(
-        "--lr-warmup-method",
-        default="constant",
-        type=str,
-        help="the warmup method (default: constant)",
-    )
-    parser.add_argument(
-        "--lr-warmup-decay", default=0.01, type=float, help="the decay for lr"
-    )
-    parser.add_argument(
-        "--lr-step-size",
-        default=30,
-        type=int,
-        help="decrease lr every step-size epochs",
-    )
-    parser.add_argument(
-        "--lr-gamma",
-        default=0.1,
-        type=float,
-        help="decrease lr by a factor of lr-gamma",
-    )
-    parser.add_argument(
-        "--lr-min",
-        default=0.0,
-        type=float,
-        help="minimum lr of lr schedule (default: 0.0)",
     )
     parser.add_argument("--print-freq", default=10, type=int, help="print frequency")
     parser.add_argument(
