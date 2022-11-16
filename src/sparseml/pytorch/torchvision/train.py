76df97ded91fa3077d106ede92a3d7466a73cebf
Corey Lowman <corey@neuralmagic.com>
2022-11-16 15:50:58 +0000

    Updating licensing of torchvision files

--- src/sparseml/pytorch/torchvision/train.py
+++ src/sparseml/pytorch/torchvision/train.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Adapted from https://github.com/pytorch/vision
+
 import datetime
 import math
 import os
