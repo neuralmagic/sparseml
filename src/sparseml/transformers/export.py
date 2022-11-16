70a65f0c2a48bd4e9382e972ee253da16da814f5
Rahul Tuli <rahul@neuralmagic.com>
2022-11-16 01:01:37 -0500

    [Refactor] export dataloader for token-classification (#1132)
    * Put model in train mode before initializing the trainer
    
    * Change UX to accept json loadable data args
    
    * propagate changes for token classification
    
    * propagate export dataloader changes for `text_classification.py` (#1137)

--- src/sparseml/transformers/export.py
+++ src/sparseml/transformers/export.py
@@ -137,15 +137,15 @@ def load_task_model(task: str, model_path: str, config: Any) -> Module:
     raise ValueError(f"unrecognized task given of {task}")
 
 
-def load_task_dataset(task: str, tokenizer, data_args: Dict[str, Any]):
+def load_task_dataset(
+    task: str, tokenizer, data_args: Dict[str, Any], model: Module, config=None
+):
     """
     :param task: the task a dataset being loaded for
     :param tokenizer: the tokenizer to use for the dataset
     :param data_args: additional data args used to create a `DataTrainingArguments`
         instance for fetching the dataset
     """
-    # TODO: fill out the function for
-    #  1) text-classification
 
     if task == "masked-language-modeling" or task == "mlm":
         from sparseml.transformers.masked_language_modeling import (
@@ -167,6 +167,38 @@ def load_task_dataset(task: str, tokenizer, data_args: Dict[str, Any]):
         data_training_args = DataTrainingArguments(**data_args)
         return get_tokenized_qa_dataset(
             data_args=data_training_args, tokenizer=tokenizer
+        )
+
+    if task == "token-classification" or task == "ner":
+        from sparseml.transformers.token_classification import (
+            DataTrainingArguments,
+            get_tokenized_token_classification_dataset,
+        )
+
+        data_training_args = DataTrainingArguments(**data_args)
+        return get_tokenized_token_classification_dataset(
+            data_args=data_training_args, tokenizer=tokenizer, model=model
+        )
+
+    if (
+        task == "sequence-classification"
+        or task == "glue"
+        or task == "sentiment-analysis"
+        or task == "text-classification"
+    ):
+
+        from sparseml.transformers.text_classification import (
+            DataTrainingArguments,
+            get_tokenized_text_classification_dataset,
+        )
+
+        data_training_args = DataTrainingArguments(**data_args)
+
+        return get_tokenized_text_classification_dataset(
+            data_args=data_training_args,
+            tokenizer=tokenizer,
+            model=model,
+            config=config,
         )
 
     raise NotImplementedError
@@ -231,7 +263,11 @@ def export_transformer_to_onnx(
     model = load_task_model(task, model_path, config)
     _LOGGER.info(f"loaded model, config, and tokenizer from {model_path}")
     tokenized_dataset = load_task_dataset(
-        task=task, tokenizer=tokenizer, data_args=data_args
+        task=task,
+        tokenizer=tokenizer,
+        data_args=data_args,
+        model=model,
+        config=config,
     )
 
     model = model.train()
