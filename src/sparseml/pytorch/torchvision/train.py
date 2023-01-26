c0d0e75e396061a83a13973f9c34e5d55efd9d7f
Abhinav Agarwalla <abhinav@neuralmagic.com>
2023-01-26 06:18:43 +0000

    Trying out accuracy fix

--- src/sparseml/pytorch/torchvision/train.py
+++ src/sparseml/pytorch/torchvision/train.py
@@ -142,8 +142,8 @@ def train_one_epoch(
         acc1, acc5 = utils.accuracy(output, target, topk=(1, 5))
         batch_size = image.shape[0]
         metric_logger.update(loss=loss.item(), lr=optimizer.param_groups[0]["lr"])
-        metric_logger.meters["acc1"].update(acc1.item(), n=batch_size)
-        metric_logger.meters["acc5"].update(acc5.item(), n=batch_size)
+        metric_logger.meters["acc1"].update(acc1.item(), n=batch_size, total=acc1)
+        metric_logger.meters["acc5"].update(acc5.item(), n=batch_size, total=acc1)
         metric_logger.meters["imgs_per_sec"].update(
             batch_size / (time.time() - start_time)
         )
