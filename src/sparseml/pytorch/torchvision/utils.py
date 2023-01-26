Moving mutiplication by 100 to accuracy computation, training log fix

src/sparseml/pytorch/torchvision/utils.py
@@ -57,7 +57,7 @@ class SmoothedValue:
 
     @property
     def global_avg(self):
-        return 100.0 * self.total / self.count
+        return self.total / self.count
 
     @property
     def max(self):
@@ -213,7 +213,7 @@ def accuracy(output, target, topk=(1,)):
         for k in topk:
             correct_k = correct[:k].flatten().sum(dtype=torch.float32)
             res.append(correct_k * (100.0 / batch_size))
-            res.append(correct_k)
+            res.append(correct_k *  100.0)
         return res
 
 