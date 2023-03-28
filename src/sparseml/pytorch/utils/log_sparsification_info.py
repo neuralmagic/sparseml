3f9fcdb83a0560fddf274cf75f9402d8a03f5705
dbogunowicz <97082108+dbogunowicz@users.noreply.github.com>
2023-03-28 14:48:23 +0200

    [ModuleSparisificationInfo] LoggingModifier (#1484)
    * initial commit
    
    * initial developement
    
    * sync with ben
    
    * prototype ready
    
    * included ben's comments
    
    * initial commit
    
    * Update src/sparseml/pytorch/utils/sparsification_info/configs.py
    
    Co-authored-by: Benjamin Fineran <bfineran@users.noreply.github.com>
    
    * Update src/sparseml/pytorch/utils/sparsification_info/configs.py
    
    Co-authored-by: Benjamin Fineran <bfineran@users.noreply.github.com>
    
    * address comments
    
    * fix quantization logic
    
    * formatting standardised to the PRD
    
    * remove identities from leaf operations
    
    * fix the Identity removal
    
    * initial commit
    
    * initial commit
    
    * checkpoint
    
    * Delete modifier_logging.py
    
    * Apply suggestions from code review
    
    * tested the modifier
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Benjamin Fineran <bfineran@users.noreply.github.com>

--- src/sparseml/pytorch/utils/log_sparsification_info.py
+++ src/sparseml/pytorch/utils/log_sparsification_info.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Optional
+
 import torch
 
 from sparseml.pytorch.utils.logger import BaseLogger
@@ -23,16 +25,20 @@ from sparseml.pytorch.utils.sparsification_info.module_sparsification_info impor
 __all__ = ["log_module_sparsification_info"]
 
 
-def log_module_sparsification_info(module: torch.nn.Module, logger: BaseLogger):
+def log_module_sparsification_info(
+    module: torch.nn.Module, logger: BaseLogger, step: Optional[float] = None
+):
     """
     Log the sparsification information for the given module to the given logger
 
     :param module: the module to log the sparsification information for
     :param logger: the logger to log the sparsification information to
+    :param step: the global step for when the sparsification information
+        is being logged. By default, is None
     """
     sparsification_info = ModuleSparsificationInfo.from_module(module)
     for tag, value in sparsification_info.loggable_items():
         if isinstance(value, dict):
-            logger.log_scalars(tag, value)
+            logger.log_scalars(tag=tag, step=step, values=value)
         else:
-            logger.log_scalar(tag, value)
+            logger.log_scalar(tag=tag, step=step, value=value)
