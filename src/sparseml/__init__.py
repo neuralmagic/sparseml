825d676e568ee04f45972de2bc53d66511d83ab6
Benjamin Fineran <bfineran@users.noreply.github.com>
2023-04-05 15:57:58 -0400

    remove sparsezoo package version check
    replaced by google analytics, version check util to be removed in future release

--- src/sparseml/__init__.py
+++ src/sparseml/__init__.py
@@ -47,15 +47,3 @@ from .analytics import sparseml_analytics as _analytics
 
 
 _analytics.send_event("python__init")
-
-try:
-    from sparsezoo.package import check_package_version as _check_package_version
-
-    _check_package_version(
-        package_name=__name__ if is_release else f"{__name__}-nightly",
-        package_version=version,
-    )
-except Exception as err:
-    print(
-        f"Need sparsezoo version above 0.9.0 to run Neural Magic's latest-version check\n{err}"
-    )
