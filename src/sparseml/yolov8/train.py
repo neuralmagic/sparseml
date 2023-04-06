2eb233a597a951fbfe93fee4fabcf6ec09251260
Corey Lowman <corey@neuralmagic.com>
2023-04-06 19:22:48 +0000

    Styling

--- src/sparseml/yolov8/train.py
+++ src/sparseml/yolov8/train.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import logging
+import os
 
 import click
 from sparseml.yolov8.trainers import SparseYOLO
