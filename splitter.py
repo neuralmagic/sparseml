ae49169c57219008ba78f66e2210d0691cd40b23
dbogunowicz <97082108+dbogunowicz@users.noreply.github.com>
2023-04-25 08:28:41 +0200

    Delete splitter.py

--- splitter.py
+++
@@ -1,7 +0,0 @@
-import onnx
-
-# Load the ONNX model
-input_path = 'opt-350m/deployment/model.onnx'
-output_path = 'model_before_split.onnx'
-onnx.utils.extract_model(input_path, output_path, [input.name for input in onnx.load(input_path).graph.input], ["onnx::Add_876"])
-print('done')
