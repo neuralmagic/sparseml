2df6d44cae7a9924eb5fef784261685a8c162c6f
Alexandre Marques <alexandre@neuralmagic.com>
2023-04-27 00:17:55 -0400

    Fix loading of zoo stubs

--- src/sparseml/yolov8/trainers.py
+++ src/sparseml/yolov8/trainers.py
@@ -531,10 +531,11 @@ class SparseYOLO(YOLO):
 
         if model_str.startswith("zoo:"):
             model = download_framework_model_by_recipe_type(
-                Model(model_str), model_suffix=".pt"
+                Model(model_str), model_suffix="pt"
             )
             self.is_sparseml_checkpoint = True
-        elif model_str.endswith(".pt"):
+
+        if model_str.endswith(".pt"):
             if os.path.exists(model_str):
                 ckpt = torch.load(model_str)
                 self.is_sparseml_checkpoint = (
