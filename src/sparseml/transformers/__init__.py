618ad7b4844f9a49d7a8a2f02d7a057628e84e14
Benjamin <ben@neuralmagic.com>
2023-05-08 16:24:53 -0400

    Revert "remove auto installs and switch to pypi forks for transformers and yolov5 (#1518)"
    This reverts commit 3688e67971ac83110dbadb5d8622ea633151601b.

--- src/sparseml/transformers/__init__.py
+++ src/sparseml/transformers/__init__.py
@@ -20,20 +20,111 @@ Tools for integrating SparseML with transformers training flows
 
 import logging as _logging
 
+import pkg_resources
 from sparseml.analytics import sparseml_analytics as _analytics
 
 
 _analytics.send_event("python__transformers__init")
 
+_EXPECTED_VERSION = "4.23.1"
+
 
 _LOGGER = _logging.getLogger(__name__)
+_NM_TRANSFORMERS_TAR_TEMPLATE = (
+    "https://github.com/neuralmagic/transformers/releases/download/"
+    "{version}/transformers-4.23.1-py3-none-any.whl"
+)
+_NM_TRANSFORMERS_NIGHTLY = _NM_TRANSFORMERS_TAR_TEMPLATE.format(version="nightly")
+
+
+def _install_transformers_and_deps():
+
+    import subprocess as _subprocess
+    import sys as _sys
+
+    import sparseml as _sparseml
+
+    nm_transformers_release = (
+        "nightly" if not _sparseml.is_release else f"v{_sparseml.version_major_minor}"
+    )
+    transformers_requirement = _NM_TRANSFORMERS_TAR_TEMPLATE.format(
+        version=nm_transformers_release
+    )
+    try:
+        _subprocess.check_call(
+            [
+                _sys.executable,
+                "-m",
+                "pip",
+                "install",
+                transformers_requirement,
+                "datasets<=1.18.4",
+                "scikit-learn",
+                "seqeval",
+            ]
+        )
+
+        import transformers as _transformers
+
+        _LOGGER.info("sparseml-transformers and dependencies successfully installed")
+    except Exception:
+        raise ValueError(
+            "Unable to install and import sparseml-transformers dependencies check "
+            "that transformers is installed, if not, install via "
+            f"`pip install {_NM_TRANSFORMERS_NIGHTLY}`"
+        )
 
 
 def _check_transformers_install():
-    # check for NM integration in transformers version
-    import transformers as _transformers
+    transformers_version = next(
+        (
+            pkg.version
+            for pkg in pkg_resources.working_set
+            if pkg.project_name.lower() == "transformers"
+        ),
+        None,
+    )
 
-    if not _transformers.NM_INTEGRATED:
+    # Either no transformers install is found or wrong version installed
+    if transformers_version != _EXPECTED_VERSION:
+        import os
+
+        if os.getenv("NM_NO_AUTOINSTALL_TRANSFORMERS", False):
+            _LOGGER.warning(
+                "Unable to import, skipping auto installation "
+                "due to NM_NO_AUTOINSTALL_TRANSFORMERS"
+            )
+            # skip any further checks
+            return
+        else:
+            _LOGGER.warning(
+                f"sparseml-transformers v{_EXPECTED_VERSION} installation not "
+                f"detected. Installing  sparseml-transformers v{_EXPECTED_VERSION} "
+                "dependencies if transformers is already  installed in the "
+                "environment, it will be overwritten. Set  environment variable "
+                "NM_NO_AUTOINSTALL_TRANSFORMERS to disable"
+            )
+            _install_transformers_and_deps()
+
+    else:
+        import transformers as _transformers
+
+        # Edge case where user has expected version of transformers installed, but
+        # not the nm integrated one
+        if not _transformers.NM_INTEGRATED:
+            _install_transformers_and_deps()
+            raise RuntimeError(
+                "Installed transformers package has been overwritten with "
+                "sparseml-transformers. Stopping process as this is likely to cause "
+                "import issues. Please re-run command"
+            )
+
+    # re check import after potential install
+    try:
+        import transformers as _transformers
+
+        assert _transformers.NM_INTEGRATED
+    except Exception:
         _LOGGER.warning(
             "the neuralmagic fork of transformers may not be installed. it can be "
             "installed via "
