ae0b966f3fb86f98eed8b8db23ab9f5a27b9dd44
Benjamin <ben@neuralmagic.com>
2023-05-12 09:11:07 -0400

    documentation + suggestions from Damian

--- src/sparseml/exporters/transforms/kv_cache/cache_keys_and_values.py
+++ src/sparseml/exporters/transforms/kv_cache/cache_keys_and_values.py
@@ -28,7 +28,7 @@ __all__ = ["CacheKeysAndValues"]
 _LOGGER = logging.getLogger(__name__)
 
 
-# idx of key node to the attention scores matmul
+# idx of key node to the key matmul
 # no great way to generically infer this from the graph since transposes can
 # be used to place it on either side of the matmul
 # hardcoding for now, will update to have a hardcoded value for each model type
@@ -47,56 +47,55 @@ class CacheKeysAndValues(OnnxTransform):
 
     Transforms
     ```
-    | QuantizeLinear
+    | Any Activation
     |     |
-    | DequantizeLinear
+    | Matmul
     |     |
-    | QuantizeLinear
+    |     ...
     |     |
-    | DequantizeLinear
+    | Softmax           Any Activation
+    |     |                   |
+    |           MatMul
     ```
     Into
     ```
-    | QuantizeLinear
+    | Concat(Key Cache + previous activation)
     |     |
-    | DequantizeLinear
+    | Matmul
+    |     |
+    |     ...
+    |     |
+    | Softmax           Concat(Value Cache + previous activation)
+    |     |                   |
+    |           MatMul
     ```
     """
 
     def transform(self, model: ModelProto) -> ModelProto:
 
-        # first matmul: RHS key
-        # second matmul: LHS value
         graph = ONNXGraph(model)
 
         # MATCH -  the matmuls that use the keys and values
-        # (attention scores and context layer)
-        attention_scores_context_layer_matmul_pairs = (
-            _find_attention_scores_context_layer_matmul_pairs(graph)
-        )
+        key_value_matmul_pairs = _find_key_value_matmul_pairs(graph)
 
         # INJECT - cache inputs/outputs + concatenation to the matmuls
         nodes_to_add = []
         inputs_to_add = []
         outputs_to_add = []
-        for idx, (attention_scores_matmul, context_layer_matmul) in enumerate(
-            attention_scores_context_layer_matmul_pairs
-        ):
+        for idx, (key_matmul, value_matmul) in enumerate(key_value_matmul_pairs):
             cache_layer_name = f"cache.{idx}"
 
             key_input_idx = _KEY_NODE_INPUT_IDX
-            value_input_idx = _value_input_idx(context_layer_matmul, graph)
+            value_input_idx = _value_input_idx(value_matmul, graph)
 
-            # TODO: inject concat node, input, and output for the matmuls for
-            # their respective keys and values
             key_concat_node, key_input_tensor, key_output_tensor = _create_cache(
-                node=attention_scores_matmul,
+                node=key_matmul,
                 graph=graph,
                 cache_input_idx=key_input_idx,
                 cache_input_name=f"{cache_layer_name}.key",
             )
             value_concat_node, value_input_tensor, value_output_tensor = _create_cache(
-                node=context_layer_matmul,
+                node=value_matmul,
                 graph=graph,
                 cache_input_idx=value_input_idx,
                 cache_input_name=f"{cache_layer_name}.value",
@@ -128,24 +127,34 @@ def _create_cache(
     cache_output_name = f"{cache_input_name}.updated"
 
     # create concat node
+    # hidden dimension must be on inside of matmul
+    # select concat axis to be -2 if cache is on LHS, -1 if RHS
+    concat_axis = -2 if cache_input_idx == 0 else -1
     concat_node = onnx.helper.make_node(
         op_type="Concat",
-        inputs=[pre_cache_input_id, cache_input_name],
+        inputs=[cache_input_name, pre_cache_input_id],
         outputs=[cache_output_name],
-        axis=-1,  # eyeballing concat on the last axis for now, TODO: determine correctness
+        axis=concat_axis,
         name=f"concat.{cache_input_name}",
     )
+
+    if concat_axis == -1:
+        cache_input_dims = ["batch_size", "heads", "hidden_dims", "cache_length"]
+        cache_output_dims = ["batch_size", "heads", "hidden_dims", "cache_length+1"]
+    else:
+        cache_input_dims = ["batch_size", "heads", "cache_length", "hidden_dims"]
+        cache_output_dims = ["batch_size", "heads", "cache_length+1", "hidden_dims"]
 
     # create graph input/output info protos
     cache_input_info = onnx.helper.make_tensor_value_info(
         cache_input_name,
         onnx.TensorProto.FLOAT,
-        ["batch_size", "hidden_dims", "cache_length"],
+        cache_input_dims,
     )
     cache_output_info = onnx.helper.make_tensor_value_info(
         cache_output_name,
         onnx.TensorProto.FLOAT,
-        ["batch_size", "hidden_dims", "cache_length+1"],
+        cache_output_dims,
     )
 
     # update all uses of the pre_cache_input_id to now reference cache output
@@ -157,29 +166,26 @@ def _create_cache(
     return concat_node, cache_input_info, cache_output_info
 
 
-def _find_attention_scores_context_layer_matmul_pairs(
+def _find_key_value_matmul_pairs(
     graph: ONNXGraph,
 ) -> List[Tuple[NodeProto, NodeProto]]:
-    attention_scores_context_layer_matmul_pairs = []
+    key_value_matmul_pairs = []
 
-    context_layer_matmuls = [
-        node for node in graph.nodes if _is_context_layer_matmul(node, graph)
-    ]
-    context_layer_matmul_names = {node.name for node in context_layer_matmuls}
+    # get value matmuls first, easiest to match
+    value_matmuls = [node for node in graph.nodes if _is_value_matmul(node, graph)]
+    value_matmul_names = {node.name for node in value_matmuls}
 
-    for context_layer_matmul in context_layer_matmuls:
-        attention_scores_matmul = _find_attention_scores_matmul_from_context_matmul(
-            context_layer_matmul, graph, context_layer_matmul_names
+    for value_matmul in value_matmuls:
+        key_matmul = _find_key_matmul_from_value_matmul(
+            value_matmul, graph, value_matmul_names
         )
-        if attention_scores_matmul is not None:
-            attention_scores_context_layer_matmul_pairs.append(
-                (attention_scores_matmul, context_layer_matmul)
-            )
-    return attention_scores_context_layer_matmul_pairs
+        if key_matmul is not None:
+            key_value_matmul_pairs.append((key_matmul, value_matmul))
+    return key_value_matmul_pairs
 
 
-def _is_context_layer_matmul(node: NodeProto, graph: ONNXGraph) -> bool:
-    # candidate context layer matmuls are non-parameterized matmuls where ont of
+def _is_value_matmul(node: NodeProto, graph: ONNXGraph) -> bool:
+    # candidate value matmuls are non-parameterized matmuls where ont of
     # the matmul inputs is the output of a Softmax
     if not is_matmul(node) or _is_parameterized_node(node, graph):
         # not a matmul or MatMul op has a parameter
@@ -189,28 +195,28 @@ def _is_context_layer_matmul(node: NodeProto, graph: ONNXGraph) -> bool:
         if not isinstance(parent, NodeProto):
             continue
         if parent.op_type == "Softmax":
-            # a parent is a Softmax node, assume this is a context layer
+            # a parent is a Softmax node, assume this is a value matmul
             return True
 
     # no parents are a softmax node
     return False
 
 
-def _find_attention_scores_matmul_from_context_matmul(
-    context_layer_matmul: NodeProto,
+def _find_key_matmul_from_value_matmul(
+    value_matmul: NodeProto,
     graph: ONNXGraph,
-    context_layer_matmul_names: Set[str],
+    value_matmul_names: Set[str],
 ) -> Optional[NodeProto]:
-    # perform a BFS up the model DAG from the context layer matmul until
-    # we find the corresponding attention score matmul
-    # the attention score matmul is assumed to be the first non-paramterized
+    # perform a BFS up the model DAG from the value matmul until
+    # we find the corresponding key matmul
+    # the key matmul is assumed to be the first non-paramterized
     # matmul we reach
     # we return None if no such matmul is found, or there is an indication that
     # we have traversed outside the self attention module (found another
-    # context layer matmul)
+    # value matmul)
 
-    seen_node_names = {context_layer_matmul.name}
-    node_queue = [context_layer_matmul]
+    seen_node_names = {value_matmul.name}
+    node_queue = [value_matmul]
 
     while node_queue:
         current_node = node_queue.pop(0)
@@ -219,15 +225,15 @@ def _find_attention_scores_matmul_from_context_matmul(
 
         if (
             is_matmul(current_node)
-            and (current_node.name != context_layer_matmul.name)
+            and (current_node.name != value_matmul.name)
             and not _is_parameterized_node(current_node, graph)
         ):
             # treat root node as regular, non matmul node
-            if current_node.name in context_layer_matmul_names:
+            if current_node.name in value_matmul_names:
                 _LOGGER.info(
-                    "first MatMul node found for context matmul %s "
-                    "was another context matmul %s",
-                    context_layer_matmul.name,
+                    "first MatMul node found for value matmul %s "
+                    "was another value matmul %s",
+                    value_matmul.name,
                     current_node.name,
                 )
                 return None
@@ -244,16 +250,16 @@ def _find_attention_scores_matmul_from_context_matmul(
 
     # no matmul matched before bottoming
     _LOGGER.info(
-        "No attention scores matmul found for context layer matmul %s",
-        context_layer_matmul.name,
+        "No key matmul found for value matmul %s",
+        value_matmul.name,
     )
     return None
 
 
-def _value_input_idx(context_layer_matmul: NodeProto, graph: ONNXGraph):
+def _value_input_idx(value_matmul: NodeProto, graph: ONNXGraph):
     # get idx of matmul that the value node is an input of
     softmax_input_idx = 0  # default to softmax being on left hand side
-    for idx, parent in enumerate(graph.get_node_parents(context_layer_matmul)):
+    for idx, parent in enumerate(graph.get_node_parents(value_matmul)):
         if isinstance(parent, NodeProto) and parent.op_type == "Softmax":
             softmax_input_idx = idx
             break
