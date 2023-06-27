8e18613303fe1468761950ba40236e29952ce028
Alexandre Marques <alexandre@neuralmagic.com>
2023-06-27 00:38:31 -0400

    Style fixes

--- tests/sparseml/exporters/transforms/test_propagate_embedding_quantization.py
+++ tests/sparseml/exporters/transforms/test_propagate_embedding_quantization.py
@@ -32,7 +32,9 @@ def onnx_model():
         "output", onnx.TensorProto.FLOAT, (1,)
     )
     scale = onnx.helper.make_tensor("scale", onnx.TensorProto.FLOAT, (1,), [1.0])
-    zero_point = onnx.helper.make_tensor("zero_point", onnx.TensorProto.INT8, (1,), [0.0])
+    zero_point = onnx.helper.make_tensor(
+        "zero_point", onnx.TensorProto.INT8, (1,), [0.0]
+    )
     starts = onnx.helper.make_tensor("starts", onnx.TensorProto.INT64, (1,), [0])
     ends = onnx.helper.make_tensor("ends", onnx.TensorProto.INT64, (1,), [1])
     pads = onnx.helper.make_tensor("pads", onnx.TensorProto.INT64, (1,), [1])
