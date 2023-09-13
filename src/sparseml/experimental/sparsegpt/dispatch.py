930728699030d50e254e68b903e1b4b9336c0c79
Alexandre Marques <alexandre@neuralmagic.com>
2023-09-13 17:29:58 -0400

    Fix model key for llama2

--- src/sparseml/experimental/sparsegpt/dispatch.py
+++ src/sparseml/experimental/sparsegpt/dispatch.py
@@ -1,4 +1,4 @@
-SUPPORTED_MODELS = ["opt", "mpt", "llama2"]
+SUPPORTED_MODELS = ["opt", "mpt", "llama-2"]
 
 
 def load_model(args, model_key: str = None, *gargs, **kwargs):
@@ -7,24 +7,24 @@ def load_model(args, model_key: str = None, *gargs, **kwargs):
         from opt import load_model as _load_model
     elif model_key == "mpt":
         from mpt import load_model as _load_model
-    elif model_key == "llama2":
+    elif model_key == "llama-2":
         from llama2 import load_model as _load_model
     else:
         raise ValueError(f"Unrecognized model key. Supported: {SUPPORTED_MODELS}")
     return _load_model(args, *gargs, **kwargs)
 
 
-def load_data(args, model_key: str = None, dataset: str = None, *gargs, **kwargs):
+def load_data(args, model_key: str = None, *gargs, **kwargs):
     model_key = _get_model_key(args) if model_key is None else model_key
     if model_key == "opt":
         from opt import load_data as _load_data
     elif model_key == "mpt":
         from mpt import load_data as _load_data
-    elif model_key == "llama2":
+    elif model_key == "llama-2":
         from llama2 import load_data as _load_data
     else:
         raise ValueError(f"Unrecognized model key. Supported: {SUPPORTED_MODELS}")
-    return _load_data(args, dataset=dataset, *gargs, **kwargs)
+    return _load_data(args, *gargs, **kwargs)
 
 
 def prepare_sparsegpt(model, dataloader, args, model_key: str = None, **kwargs):
@@ -33,7 +33,7 @@ def prepare_sparsegpt(model, dataloader, args, model_key: str = None, **kwargs):
         from opt import prepare_sparsegpt as _prepare_sparsegpt
     elif model_key == "mpt":
         from mpt import prepare_sparsegpt as _prepare_sparsegpt
-    elif model_key == "llama2":
+    elif model_key == "llama-2":
         from llama2 import prepare_sparsegpt as _prepare_sparsegpt
     else:
         raise ValueError(f"Unrecognized model key. Supported: {SUPPORTED_MODELS}")
