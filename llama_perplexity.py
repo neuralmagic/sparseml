ddf35be89b1f9f70ff5052835e47a4b48ec6adb4
Sara Adkins <sara@neuralmagic.com>
2023-09-21 14:38:39 -0400

    remove local files

--- llama_perplexity.py
+++
@@ -1,8 +0,0 @@
-from deepsparse.transformers.evaluator import TransformersEvaluator
-
-
-model = "/home/sadkins/sparseml/deploy_llama/deployment"
-dataset = "wikitext2"
-evaluator = TransformersEvaluator(model=model, dataset=dataset)
-evaluator.evaluate()
-print(evaluator.get_results())
