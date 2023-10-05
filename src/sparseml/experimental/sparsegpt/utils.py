d727db4c1da8871993339606dd609c5ef1e646b2
Alexandre Marques <alexandre@neuralmagic.com>
2023-10-05 14:25:34 -0400

    Quality fixes

--- src/sparseml/experimental/sparsegpt/utils.py
+++ src/sparseml/experimental/sparsegpt/utils.py
@@ -355,8 +355,11 @@ def get_openplatypus(nsamples, seed, seqlen, model, split):
         traindata = traindata[:nsamples]
 
     alpaca_template = {
-        "prompt_input": "Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n\n### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n",
-        "prompt_no_input": "Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\n{instruction}\n\n### Response:\n",
+        "prompt_input": "Below is an instruction that describes a task, paired with an input that provides further "
+                        "context. Write a response that appropriately completes the request.\n\n### Instruction:\n{"
+                        "instruction}\n\n### Input:\n{input}\n\n### Response:\n",
+        "prompt_no_input": "Below is an instruction that describes a task. Write a response that appropriately "
+                           "completes the request.\n\n### Instruction:\n{instruction}\n\n### Response:\n",
     }
 
     from transformers import AutoTokenizer
