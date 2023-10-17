b6cc013cf049423fdc0cbc27747329e80f92c8ad
Sara Adkins <sara@neuralmagic.com>
2023-10-17 14:29:47 -0400

    reload about QAT adjustment

--- src/sparseml/transformers/sparsification/obcq/export.py
+++ src/sparseml/transformers/sparsification/obcq/export.py
@@ -70,6 +70,7 @@ import logging
 import os
 import shutil
 from typing import Any, Dict, List, Optional, Union
+import torch
 
 from torch.nn import Module
 from transformers import AutoConfig, AutoTokenizer
@@ -82,6 +83,9 @@ from sparseml.pytorch.utils import export_onnx
 from sparseml.transformers.utils import SparseAutoModel
 from sparseml.transformers.utils.helpers import RECIPE_NAME
 from sparsezoo.utils.onnx import EXTERNAL_ONNX_DATA_NAME
+from sparseml.pytorch.sparsification.quantization.helpers import (
+    initialize_channel_wise_scale_zp,
+)
 
 
 __all__ = ["export_transformer_to_onnx", "load_task_model"]
@@ -100,6 +104,83 @@ OPTIONAL_DEPLOYMENT_FILES.extend(OPT_TOKENIZER_FILES)
 
 
 _LOGGER = logging.getLogger(__name__)
+
+def _reload_model_state(model, load_path: str, orig_state_dict: Dict[str, Any]):
+    """
+    Reload the weights after model arch changes due to recipe application
+    Return True if weights are successfully reloaded; False otherwise
+    """
+    invalid_load_path = not load_path or not os.path.isdir(load_path)
+    files = os.listdir(load_path) if not invalid_load_path else []
+    weight_files = [
+        os.path.join(load_path, f)
+        for f in files
+        if f.startswith("pytorch_model") and f.endswith("bin")
+    ]
+    if not weight_files:
+        _LOGGER.warning(
+            "Model state was not reloaded for SparseML: "
+            f"could not find model weights for {load_path}"
+        )
+        return False
+
+    # PerChannel quantization observers initialize variables
+    # to dummy shapes that do not match the ones saved in
+    # state_dict.
+    # Need to reshape these variables in order to load state_dict
+    # properly.
+    initialize_channel_wise_scale_zp(model)
+
+    current_state_dict = model.state_dict()
+
+    if set(orig_state_dict.keys()) == set(current_state_dict):
+        # no change in keys, ignore reload
+        return False
+
+    # change in keys due to architecture changes, reload statedict
+    loaded_state_dict = {}
+    for f in weight_files:
+        dd = torch.load(os.path.join(load_path, f), map_location="cpu")
+        loaded_state_dict.update(dd)
+
+    _, missing, unexpected, mismatched, _, _ = model._load_pretrained_model(
+        model=model,
+        state_dict=loaded_state_dict,
+        loaded_keys=list(loaded_state_dict.keys()),
+        resolved_archive_file=None,
+        pretrained_model_name_or_path=load_path,
+        _fast_init=False,
+    )
+
+    if missing:
+        _LOGGER.warning(
+            "Missing keys found when reloading model state for SparseML recipe:"
+            f"{missing}"
+        )
+
+    if unexpected:
+        _LOGGER.warning(
+            f"Unexpected keys found when reloading model state for SparseML recipe:"
+            f"{unexpected}"
+        )
+
+    if mismatched:
+        _LOGGER.warning(
+            f"Mismatched keys found when reloading model state for SparseML recipe:"
+            f"{mismatched}"
+        )
+
+    total_loaded = len(current_state_dict) - (len(missing) if len(missing) else 0)
+    _LOGGER.info(
+        f"Reloaded {total_loaded} model params for SparseML Recipe from {load_path}"
+    )
+    SparseAutoModel.log_model_load(
+        model,
+        load_path,
+        model_type="student",
+        delayed_load=False,
+    )
+    return True
 
 
 def load_task_model(
@@ -290,6 +371,8 @@ def export_transformer_to_onnx(
         )
         recipe_path = None
 
+    orig_state_dict = model.state_dict()
+
     sml.create_session()
     sml.pre_initialize_structure(
         model=model, recipe=recipe_path, framework=Framework.pytorch
@@ -304,6 +387,13 @@ def export_transformer_to_onnx(
             else f"a staged recipe with {num_stages} stages"
         )
         _LOGGER.info(f"Applied {msg} to the model at {model_path}")
+
+    # reload the state dict for the model now that architecture matches expected
+    if _reload_model_state(model, model_path, orig_state_dict):
+        _LOGGER.info(
+            "Reloaded model state after SparseML recipe structure modifications "
+            f"from {model_path}"
+        )
 
     # create fake model input
     inputs = tokenizer(
