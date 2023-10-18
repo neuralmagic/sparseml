1b86cebd917cca4bf9d91fd782ca50115ccfdc3e
Rahul Tuli <rahul@neuralmagic.com>
2023-10-18 08:23:02 -0400

    Add more tests to check valid modifiers are created (#1774)

--- tests/sparseml/core/recipe/test_recipe.py
+++ tests/sparseml/core/recipe/test_recipe.py
@@ -17,8 +17,9 @@ import tempfile
 import pytest
 import yaml
 
+from sparseml.core.framework import Framework
 from sparseml.core.recipe import Recipe
-from tests.sparseml.helpers import valid_recipe_strings
+from tests.sparseml.helpers import should_skip_pytorch_tests, valid_recipe_strings
 
 
 @pytest.mark.parametrize("recipe_str", valid_recipe_strings())
@@ -39,10 +40,42 @@ def test_recipe_create_instance_accepts_valid_recipe_file(recipe_str):
 @pytest.mark.parametrize("recipe_str", valid_recipe_strings())
 def test_serialization(recipe_str):
     recipe_instance = Recipe.create_instance(recipe_str)
-    serialized_recipe_str = recipe_instance.yaml()
-    recipe_from_serialized = Recipe.create_instance(serialized_recipe_str)
+    recipe_from_serialized = Recipe.create_instance(recipe_instance.yaml())
 
     expected_dict = recipe_instance.dict()
     actual_dict = recipe_from_serialized.dict()
 
     assert expected_dict == actual_dict
+
+
+@pytest.mark.skipif(
+    should_skip_pytorch_tests(),
+    reason="Skipping pytorch tests either torch is not installed or "
+    "NM_ML_SKIP_PYTORCH_TESTS is set",
+)
+def test_recipe_creates_correct_modifier():
+    start = 1
+    end = 10
+    targets = "__ALL_PRUNABLE__"
+
+    yaml_str = f"""
+        test_stage:
+            pruning_modifiers:
+                ConstantPruningModifier:
+                    start: {start}
+                    end: {end}
+                    targets: {targets}
+        """
+
+    recipe_instance = Recipe.create_instance(yaml_str)
+
+    stage_modifiers = recipe_instance.create_modifier(framework=Framework.pytorch)
+    assert len(stage_modifiers) == 1
+    assert len(modifiers:=stage_modifiers[0].modifiers) == 1
+    from sparseml.modifiers.pruning.constant.pytorch import (
+        ConstantPruningModifierPyTorch,
+    )
+
+    assert isinstance(modifier := modifiers[0], ConstantPruningModifierPyTorch)
+    assert modifier.start == start
+    assert modifier.end == end
