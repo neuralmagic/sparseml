7ac60a45a509d84657f785daa2b664a80e81fb26
rahul-tuli <rahul@neuralmagic.com>
2023-10-27 09:24:20 -0400

    Base Structure

--- src/sparseml/modifiers/pruning/wanda/pytorch.py
+++ src/sparseml/modifiers/pruning/wanda/pytorch.py
@@ -15,8 +15,10 @@
 
 from typing import Any
 
+import torch
+
 from sparseml.core import State
-from sparseml.core.event import Event, EventType
+from sparseml.core.event import Event
 from sparseml.modifiers.pruning.magnitude.pytorch import MagnitudePruningModifierPyTorch
 from sparseml.modifiers.pruning.utils.pytorch.mask_factory import PruningMaskCreatorArgs
 from sparseml.modifiers.pruning.wanda.base import WandaPruningModifier
@@ -33,6 +35,7 @@ class WandaPruningModifierPyTorch(
         Large Language Models"
     """
 
+    # start = -1
     calibration_dataloader_: Any = None
     calibration_function_: Any = None
 
@@ -40,7 +43,44 @@ class WandaPruningModifierPyTorch(
         if self.apply_globally:
             raise NotImplementedError("global pruning not implemented yet for PyTorch")
 
-        return super().on_initialize(state, **kwargs)
+        super().on_initialize(state, **kwargs)
+        if self.end and self.end != -1:
+            raise ValueError(
+                "end_epoch is disabled for WandaPruningModifier and can only be set to"
+                " -1 or None. Given {}".format(self.end)
+            )
+
+        if _start := self.calculate_start() == -1:  # one-shot
+            return self.one_shot(state)
+
+        raise ValueError(f"start must be -1 for WandaPruningModifier. Given {_start}")
+
+    def one_shot(self, state: State):
+        """
+        One-shot pruning implementation for Wanda.
+        """
+        self.on_start(state, state.start_event)
+        input_activations = self._collect_input_activations(state)
+
+        # set the sparsity to the final sparsity
+        sparsity = self.final_sparsity
+
+        for (
+            layer_param_name,
+            parameterized_layer,
+        ) in self.parameterized_layers_.items():
+            mask = self.mask_creator_function_(
+                PruningMaskCreatorArgs(
+                    parameter=parameterized_layer.param,
+                    sparsity=sparsity,
+                    scores=self._get_wanda_scores(
+                        parameterized_layer, input_activations
+                    ),
+                )
+            )
+            self.update_mask(layer_param_name, mask)
+            self.apply_mask_gradient(layer_param_name)
+            self.apply_mask_weight(layer_param_name)
 
     def on_start(self, state: State, event: Event, **kwargs):
         sparsity = self.scheduler_function_(event, state)
@@ -59,24 +99,33 @@ class WandaPruningModifierPyTorch(
 
         self.enable_masks()
 
-    def on_update(self, state: State, event: Event, **kwargs):
-        if event.type_ == EventType.BATCH_START:
-            sparsity = self.scheduler_function_(event, state)
-            if sparsity != self.current_sparsity_:
-                self.current_sparsity_ = sparsity
+    def _get_wanda_scores(self, parameterized_layer, input_activations):
+        """
+        Calculate the Wanda scores for a given layer.
+        """
 
-                for (
-                    layer_param_name,
-                    parameterized_layer,
-                ) in self.parameterized_layers_.items():
-                    mask = self.mask_creator_function_(
-                        PruningMaskCreatorArgs(
-                            parameter=parameterized_layer.param,
-                            sparsity=sparsity,
-                            # TODO: Update scores
-                            scores=parameterized_layer.param.data.abs(),
-                        )
-                    )
-                    self.update_mask(layer_param_name, mask)
-        else:
-            self._update_masks(event)
+        weight = parameterized_layer.param.data
+        input_activation = input_activations[parameterized_layer.name]
+        l2_norm = torch.sqrt(input_activation.scaler_row.reshape(-1, 1))
+        return wanda_score(weight=weight, activation_l2_norm=l2_norm)
+
+    def _collect_input_activations(state):
+        """
+        returns a dict mapping of the following form:
+
+        layer_param_name -> input_activation
+
+        where the input_activation must be euclidean norm (w/o
+        sqrt) of the input to the layer, and the layer_param_name is
+        the name of the layer parameter in the model.
+        The euclidean norm does not need to
+        """
+        raise NotImplementedError("TODO: implement this")
+
+
+def wanda_score(weight, activation_l2_norm):
+    """
+    Calculate the Wanda score for a given weight
+    and scaler row.
+    """
+    return weight.abs() * activation_l2_norm
--- tests/sparseml/pytorch/modifiers/pruning/wanda/test_pytorch.py
+++ tests/sparseml/pytorch/modifiers/pruning/wanda/test_pytorch.py
@@ -13,9 +13,15 @@
 # limitations under the License.
 
 
+import pytest
+import torch
+
 from sparseml.core.factory import ModifierFactory
 from sparseml.core.framework import Framework
-from sparseml.modifiers.pruning.wanda.pytorch import WandaPruningModifierPyTorch
+from sparseml.modifiers.pruning.wanda.pytorch import (
+    WandaPruningModifierPyTorch,
+    wanda_score,
+)
 from tests.sparseml.modifiers.conf import setup_modifier_factory
 
 
@@ -31,3 +37,33 @@ def test_wanda_registered():
         **kwargs,
     )
     assert isinstance(quant_obj, WandaPruningModifierPyTorch)
+
+
+def _get_weight_l2_norm_and_expected_score():
+    weight = torch.tensor(
+        [
+            [4, 0, 1, -1],
+            [3, -2, -1, -3],
+            [-3, 1, 0, 2],
+        ]
+    )
+
+    l2_norm = torch.tensor([1, 2, 8, 3])
+
+    expected = torch.tensor(
+        [
+            [4, 0, 8, 3],
+            [3, 4, 8, 9],
+            [3, 2, 0, 6],
+        ]
+    )
+
+    return weight, l2_norm, expected
+
+
+@pytest.mark.parametrize(
+    "weight, l2_norm, expected_scores", [_get_weight_l2_norm_and_expected_score()]
+)
+def test_wanda_scores(weight, l2_norm, expected_scores):
+    actual_scores = wanda_score(weight, l2_norm)
+    assert torch.equal(actual_scores, expected_scores), "wanda scores are not correct"
