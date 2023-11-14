[Bug Fix] Add Precision Argument for Disabling bf16 (#1827)
* cast to float before quantization calibration

* add precision casting as argument

* reverting debug changes:

* display precision choices

* additional precision aliases

---------

Co-authored-by: Rahul Tuli <rahul@neuralmagic.com>

src/sparseml/transformers/sparsification/obcq/obcq.py
@@ -18,6 +18,7 @@ import os
 from pathlib import Path
 from typing import Optional
 
+import torch
 from torch.nn import Module
 from transformers import AutoConfig
 
@@ -38,6 +39,7 @@ __all__ = ["one_shot"]
 _LOGGER = logging.getLogger(__name__)
 SUPPORTED_DATASETS = TransformersDataset.registered_names()
 SUPPORTED_MODELS = ["opt", "llama", "mistral"]
+SUPPORTED_PRECISION = ["auto", "half", "full", "float16", "bfloat16", "float32"]
 
 
 def one_shot(
@@ -47,6 +49,7 @@ def one_shot(
     device: str = "cuda:0",
     deploy_dir: Optional[str] = ".",
     recipe_file: Optional[str] = None,
+    precision: str = "auto",
     eval_data: Optional[str] = None,
     do_save: Optional[bool] = False,
 ) -> Module:
@@ -59,6 +62,7 @@ def one_shot(
     :param device: Device (cuda:index or cpu) to use for computation
     :param deploy_dir: The output directory to save the model to
     :param recipe_file: recipe containing SparseGPT configuration
+    :param precision: precision to load model as, either auto, half or full
     :param eval_data: dataset to use for perplexity evalaution, or none to skip
     :param do_save: whether to save the output model to disk
 
@@ -88,7 +92,8 @@ def one_shot(
         forward_fn = llama_forward
     else:
         raise ValueError(f"model_path={model_path} should be one of {SUPPORTED_MODELS}")
-    model = model_loader_fn(model_path)
+    torch_dtype = _parse_dtype(precision)
+    model = model_loader_fn(model_path, torch_dtype=torch_dtype)
 
     if dataset_name not in SUPPORTED_DATASETS:
         raise ValueError(
@@ -137,6 +142,18 @@ def one_shot(
     return model
 
 
+def _parse_dtype(dtype_arg):
+    dtype = "auto"  # get precision from model by default
+    if dtype_arg == "half" or dtype_arg == "float16":
+        dtype = torch.float16
+    elif dtype_arg == "bfloat16":
+        dtype = torch.bfloat16
+    elif dtype_arg == "full" or dtype_arg == "float32":
+        dtype = torch.float32
+
+    return dtype
+
+
 def _save(model, tokenizer, save_path, recipe_path):
     model.save_pretrained(save_path)
     tokenizer.save_pretrained(save_path)
@@ -164,6 +181,13 @@ if __name__ == "__main__":
     parser.add_argument("--deploy-dir", type=str, default=".")
     parser.add_argument("--recipe", type=str, default=None)
     parser.add_argument(
+        "--precision",
+        type=str,
+        choices=SUPPORTED_PRECISION,
+        default="auto",
+        help="Precision to cast model weights to, default to auto",
+    )
+    parser.add_argument(
         "--eval", type=str, default=None, help="Optional dataset for perplexity eval"
     )
     parser.add_argument(
@@ -179,6 +203,7 @@ if __name__ == "__main__":
         num_samples=args.nsamples,
         device=args.device,
         recipe_file=args.recipe,
+        precision=args.precision,
         eval_data=args.eval,
         do_save=args.save,
     )