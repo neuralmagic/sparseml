9b7cc6eb9b99bd56d19d04959442436360bd8a69
rahul-tuli <rahul@neuralmagic.com>
2023-11-15 07:38:36 -0500

    Add wanda base

---
+++ src/sparseml/modifiers/pruning/wanda/base.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from typing import List, Union
+
+from sparseml.core import Modifier
+from sparseml.core.state import State
+from sparseml.utils import ALL_TOKEN
+
+
+__all__ = ["WandaPruningModifier"]
+
+
+class WandaPruningModifier(Modifier):
+    """
+    Modifier for applying the one-shot WANDA algorithm to a model
+    from the paper: https://arxiv.org/abs/2306.11695
+    """
+    
+    sparsity: Union[float, List[float]]
+    block_size: int
+    targets: Union[str, List[str], None] = ALL_TOKEN
+    mask_structure: str = "unstructured"
+
+    def on_initialize_structure(self, state: State, **kwargs):
+        pass  # nothing needed for this modifier
+
+    def compressible_layers(self) -> List:
+        """
+        Retrieves the modules corresponding to a list of
+        compressible layer names
+
+        :return: list of Pytorch modules to compress
+        """
+        compressible_dict = self.model.get_layers(self.targets)
+        return [v for _, v in compressible_dict.items()]
---
+++ tests/sparseml/modifiers/pruning/wanda/test_base.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from sparseml.core.factory import ModifierFactory
+from sparseml.core.framework import Framework
+from sparseml.modifiers.pruning.wanda.base import WandaPruningModifier
+from tests.sparseml.modifiers.conf import setup_modifier_factory
+
+
+def test_wanda_is_registered():
+
+    kwargs = dict(
+        sparsity=0.5,
+        block_size=128,
+        targets="__ALL_PRUNABLE__",
+    )
+    setup_modifier_factory()
+    type_ = ModifierFactory.create(
+        type_="WandaPruningModifier",
+        framework=Framework.general,
+        allow_experimental=False,
+        allow_registered=True,
+        **kwargs,
+    )
+
+    assert isinstance(
+        type_, WandaPruningModifier
+    ), "PyTorch ConstantPruningModifier not registered"
