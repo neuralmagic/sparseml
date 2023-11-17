275dffc58f84b742d16260cb5a36f1869df14385
Abhinav Agarwalla <abhinav@neuralmagic.com>
2023-11-17 16:46:11 -0500

    style fix

--- src/sparseml/transformers/data/__init__.py
+++ src/sparseml/transformers/data/__init__.py
@@ -15,8 +15,8 @@
 # flake8: noqa
 from .base_llm import TransformersDataset
 from .c4 import *
+from .evolcodealpaca import *
 from .gsm8k import *
 from .open_platypus import *
 from .ptb import *
 from .wikitext import *
-from .evolcodealpaca import *
