c35206deae4de6dec560f343ca2dedd36aade4df
Sara Adkins <sara@neuralmagic.com>
2024-01-09 11:23:45 -0500

    [One Shot] Specify recipe_args on CLI  (#1902)
    * add cli arg for recipe_args
    
    * style
    
    * fix for output recipe
    
    * adding recipe args to finetuning
    
    * distillation fix

--- src/sparseml/transformers/finetune/text_generation.py
+++ src/sparseml/transformers/finetune/text_generation.py
@@ -104,6 +104,13 @@ def parse_args(**kwargs):
     else:
         model_args, data_args, training_args = parser.parse_dict(kwargs)
 
+    if training_args.recipe_args is not None:
+        arg_dict = {}
+        for recipe_arg in training_args.recipe_args:
+            key, value = recipe_arg.split("=")
+            arg_dict[key] = value
+        training_args.recipe_args = arg_dict
+
     return model_args, data_args, training_args
 
 
@@ -166,6 +173,10 @@ def main(
         revision=model_args.model_revision,
         use_auth_token=True if model_args.use_auth_token else None,
     )
+    teacher_config = AutoConfig.from_pretrained(
+        training_args.distill_teacher,
+        use_auth_token=True if model_args.use_auth_token else None,
+    )
 
     model_kwargs = {
         "config": config,
@@ -175,6 +186,7 @@ def main(
         "torch_dtype": parse_dtype(model_args.precision),
     }
     teacher_kwargs = {
+        "config": teacher_config,
         "cache_dir": model_args.cache_dir,
         "use_auth_token": True if model_args.use_auth_token else None,
     }
