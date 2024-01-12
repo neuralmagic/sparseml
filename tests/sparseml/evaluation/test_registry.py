35c53285247863ab9680f93a7554493a57284a5d
rahul-tuli <rahul@neuralmagic.com>
2024-01-12 08:43:14 -0500

    Add SparseMLEvalRegistry (#1999)
    Move to a config based system to load
    eval integrations

---
+++ tests/sparseml/evaluation/test_registry.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from sparseml.evaluation.registry import SparseMLEvaluationRegistry
+
+
+def test_resolve_returns_value_from_registry(mocker):
+    mock_collect_integrations = mocker.patch(
+        "sparseml.evaluation.registry.collect_integrations"
+    )
+    mock_get_value_from_registry = mocker.patch.object(
+        SparseMLEvaluationRegistry, "get_value_from_registry", return_value="value"
+    )
+    name = "integration_name"
+
+    result = SparseMLEvaluationRegistry.resolve(name)
+
+    mock_collect_integrations.assert_called_once()
+    mock_get_value_from_registry.assert_called_once_with(name=name)
+    assert result == "value"
