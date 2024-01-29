e9d74b5f1dea1193b5a2ea4b6194695529aa9c96
dbogunowicz <97082108+dbogunowicz@users.noreply.github.com>
2024-01-29 17:27:11 +0100

    [Export Refactor] Support for external data chunks (#1983)
    * initial commit
    
    * initial commit
    
    * fix tests
    
    * fix tests and address comments
    
    * take michaels suggestions into consideration
    
    * Apply suggestions from code review
    
    * initial commit
    
    * finish the rebase
    
    * quality
    
    * add CLI support
    
    * add logic for digesting external_data_chunk_size_mb
    
    * remove prohibitively large tests(2)
    
    * [Export Refactor] Decouple v1 trainer with v2 LLM initialization (#1993)
    
    * initial commit. most tests pass, need to add few unit tests
    
    * refactor and remove heavy-weight test
    
    * remove model from the PR
    
    * fix tests
    
    * Update src/sparseml/export/helpers.py
    
    Co-authored-by: Sara Adkins <sara@neuralmagic.com>
    
    * Update src/sparseml/export/helpers.py
    
    Co-authored-by: Sara Adkins <sara@neuralmagic.com>
    
    * add unit tests for create_fake_dataloader
    
    ---------
    
    Co-authored-by: Sara Adkins <sara@neuralmagic.com>

--- src/sparseml/transformers/integration_helper_functions.py
+++ src/sparseml/transformers/integration_helper_functions.py
@@ -18,7 +18,6 @@ from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 import torch
 from pydantic import Field
-from transformers import AutoTokenizer
 
 from sparseml.export.export_data import create_data_samples as create_data_samples_
 from sparseml.export.helpers import apply_optimizations as apply_optimizations_onnx
@@ -26,7 +25,7 @@ from sparseml.integration_helper_functions import (
     IntegrationHelperFunctions,
     Integrations,
 )
-from sparseml.transformers.sparsification.trainer import Trainer
+from sparseml.transformers.finetune.data.data_helpers import format_calibration_data
 from sparseml.transformers.utils.helpers import (
     ALL_TASK_NAMES,
     MANDATORY_DEPLOYMENT_FILES,
@@ -34,6 +33,7 @@ from sparseml.transformers.utils.helpers import (
     NLG_OPTIONAL_DEPLOYMENT_FILES,
     OPTIONAL_DEPLOYMENT_FILES,
     TaskNames,
+    create_fake_dataloader,
     remove_past_key_value_support_from_config,
     resolve_sequence_length,
 )
@@ -113,7 +113,6 @@ def create_model(
         device=device,
     )
 
-    validation_dataset = None
     data_args = _parse_data_args(data_args)
 
     if data_args:
@@ -125,39 +124,42 @@ def create_model(
             config=config,
             split="validation",
         )
-        if task in TaskNames.text_generation.value and not dataset_with_labels:
-            validation_dataset = validation_dataset.remove_columns("labels")
 
-    trainer = initialize_trainer(model, source_path, validation_dataset)
+        if task in TaskNames.text_generation.value:
+            # text-generation datasets have a separate
+            # logic for creating a dataloader
+            if not dataset_with_labels:
+                validation_dataset = validation_dataset.remove_columns("labels")
+            data_loader = format_calibration_data(tokenized_dataset=validation_dataset)
+            input_names = validation_dataset.column_names
+
+        else:
+            trainer = initialize_trainer(model, source_path, validation_dataset)
+            data_loader = trainer.get_eval_dataloader()
+            input_names = list(next(trainer._get_fake_dataloader(1, tokenizer)).keys())
+
+    else:
+        # if no data_args are provided, create a fake dataloader
+        data_loader, input_names = create_fake_dataloader(
+            model, tokenizer, num_samples=1
+        )
 
     return model, dict(
-        trainer=trainer,
-        tokenizer=tokenizer,
-        input_names=list(next(trainer._get_fake_dataloader(1, tokenizer)).keys()),
+        data_loader=data_loader, tokenizer=tokenizer, input_names=input_names
     )
 
 
 def create_dummy_input(
-    trainer: Optional[Trainer] = None,
-    tokenizer: Optional[AutoTokenizer] = None,
+    data_loader: torch.utils.data.DataLoader,
     **kwargs,
 ) -> torch.Tensor:
-    if trainer.eval_dataset is not None:
-        data_loader = trainer.get_eval_dataloader()
-    else:
-        if not tokenizer:
-            raise ValueError(
-                "Tokenizer is needed to generate "
-                "fake sample inputs when the trainer is "
-                "not initialized with an eval dataset"
-            )
-        data_loader = trainer._get_fake_dataloader(num_samples=1, tokenizer=tokenizer)
+
     return next(iter(data_loader))
 
 
 def create_data_samples(
     num_samples: int,
-    trainer: Trainer,
+    data_loader: torch.utils.data.DataLoader,
     model: Optional["torch.nn.Module"] = None,
     **kwargs,
 ):
@@ -166,14 +168,9 @@ def create_data_samples(
             "For exporting samples for transformers integration,"
             "batch size is ignored (equal to 1)"
         )
-    if trainer.eval_dataset is None:
-        raise ValueError(
-            "Attempting to create data samples without an eval dataloader. "
-            "Initialize a trainer with an eval dataset"
-        )
 
     return create_data_samples_(
-        data_loader=trainer.get_eval_dataloader(), model=model, num_samples=num_samples
+        data_loader=data_loader, model=model, num_samples=num_samples
     )
 
 
