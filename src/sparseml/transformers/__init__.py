90a420244d812f3adc1581b9f6edbf3c12e33d30
dbogunowicz <damian@neuralmagic.com>
2024-01-30 14:25:04 +0000

    really avoid circular important avoidance

--- src/sparseml/transformers/__init__.py
+++ src/sparseml/transformers/__init__.py
@@ -54,5 +54,7 @@ def _check_transformers_install():
 
 _check_transformers_install()
 
-from .export import *
+# isort: skip_file
+# (import order matters for circular import avoidance)
 from .utils import *
+from .export import *
