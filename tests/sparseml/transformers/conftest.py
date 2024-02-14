0c5d65a15fa6814ab76eef2355bdfa92b6bdbbbe
Sara Adkins <sara@neuralmagic.com>
2024-02-14 18:08:32 +0000

    more cache

--- tests/sparseml/transformers/conftest.py
+++ tests/sparseml/transformers/conftest.py
@@ -19,6 +19,7 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def run_before_and_after_tests(tmp_path):
-    #os.environ["TRANSFORMERS_CACHE"] = str(tmp_path)
-    #os.environ["HF_DATASETS_CACHE"] = str(tmp_path)
+    os.environ["TRANSFORMERS_CACHE"] = str(tmp_path) / "transformers"
+    os.environ["HF_DATASETS_CACHE"] = str(tmp_path) / "datasets"
+    os.environ["SPARSEZOO_MODELS_PATH"] = str(tmp_path) / "sparsezoo"
     yield
