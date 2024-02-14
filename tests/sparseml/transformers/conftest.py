548ee63df62ab5a109ee48d6cbbfb4913ad2edb8
Sara Adkins <sara@neuralmagic.com>
2024-02-14 15:37:11 +0000

    revert stuff

--- tests/sparseml/transformers/conftest.py
+++ tests/sparseml/transformers/conftest.py
@@ -19,6 +19,6 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def run_before_and_after_tests(tmp_path):
-    os.environ["TRANSFORMERS_CACHE"] = str(tmp_path)
-    os.environ["HF_DATASETS_CACHE"] = str(tmp_path)
+    #os.environ["TRANSFORMERS_CACHE"] = str(tmp_path)
+    #os.environ["HF_DATASETS_CACHE"] = str(tmp_path)
     yield
