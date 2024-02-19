d31e13c279009fa7123acddd974047c094dc7dab
George <george@neuralmagic.com>
2024-02-19 12:42:23 -0500

    Goin custom bug (#2095)
    * remove column should not depend on preprocessing func
    
    * populate delete columsn by default
    
    * use set
    
    * comment

--- src/sparseml/transformers/finetune/data/custom.py
+++ src/sparseml/transformers/finetune/data/custom.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from copy import deepcopy
+from typing import List
 
 from datasets.dataset_dict import DatasetDict
 
@@ -47,6 +48,10 @@ class CustomDataset(TextGenerationDataset):
 
         raw_dataset: DatasetDict = super().get_raw_dataset()
 
+        self.remove_columns = (
+            self.remove_columns or self.get_remove_columns_from_dataset(raw_dataset)
+        )
+
         if self.remove_columns is not None:
             raw_dataset = self.map(
                 raw_dataset,
@@ -66,3 +71,14 @@ class CustomDataset(TextGenerationDataset):
             )
 
         return raw_dataset
+
+    def get_remove_columns_from_dataset(self, raw_dataset: DatasetDict) -> List[str]:
+        """Remove redandant columns from the dataset for processing"""
+        remove_columns = set()
+        for datasets in raw_dataset.values():
+            for feature in datasets.features.keys():
+                remove_columns.add(feature)
+
+        remove_columns.remove(self.text_column)
+
+        return list(remove_columns)
