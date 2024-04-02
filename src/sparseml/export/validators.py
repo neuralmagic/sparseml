5efd7b19c1b7f3750a4e68df52a8f112b363cd78
dbogunowicz <damian@neuralmagic.com>
2024-04-02 10:19:51 +0000

    fixing tests 1

--- src/sparseml/export/validators.py
+++ src/sparseml/export/validators.py
@@ -17,7 +17,7 @@ import logging
 import os.path
 from collections import OrderedDict
 from pathlib import Path
-from typing import Callable, List, Optional, Union
+from typing import Callable, List, Optional
 from typing import OrderedDict as OrderedDictType
 
 import numpy
