98d2d7e18ae1a98c1e46dd45dacb94402274af7d
Dipika Sikka <dipikasikka1@gmail.com>
2024-04-09 20:33:35 +0000

    quality

--- tests/sparseml/pytorch/oneshot/test_cli.py
+++ tests/sparseml/pytorch/oneshot/test_cli.py
@@ -1,20 +1,35 @@
+# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import shutil
 import unittest
-from pathlib import Path
-from parameterized import parameterized_class
-from tests.testing_utils import parse_params, requires_torch
 from subprocess import PIPE, STDOUT, run
 
 import pytest
 
-import shutil
+from parameterized import parameterized_class
+from tests.testing_utils import parse_params, requires_torch
+
 
 CONFIGS_DIRECTORY = "tests/sparseml/pytorch/oneshot/oneshot_configs"
+
 
 @pytest.mark.smoke
 @pytest.mark.integration
 @requires_torch
 @parameterized_class(parse_params(CONFIGS_DIRECTORY))
-class TestOneShotInputs(unittest.TestCase):
+class TestOneShotCli(unittest.TestCase):
     model = None
     dataset = None
     recipe = None
@@ -34,7 +49,7 @@ class TestOneShotInputs(unittest.TestCase):
             "--recipe",
             self.recipe,
             "--num_calibration_samples",
-            "10"
+            "10",
         ]
 
         res = run(cmd, stdout=PIPE, stderr=STDOUT, check=False, encoding="utf-8")
--- tests/sparseml/pytorch/oneshot/test_inputs.py
+++ tests/sparseml/pytorch/oneshot/test_inputs.py
@@ -1,13 +1,28 @@
+# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import shutil
 import unittest
-from pathlib import Path
-from parameterized import parameterized_class
-from tests.testing_utils import parse_params, requires_torch
 
 import pytest
 
-import shutil
+from parameterized import parameterized_class
+from tests.testing_utils import parse_params, requires_torch
+
 
 CONFIGS_DIRECTORY = "tests/sparseml/pytorch/oneshot/oneshot_configs"
+
 
 @pytest.mark.smoke
 @pytest.mark.integration
@@ -19,10 +34,14 @@ class TestOneShotInputs(unittest.TestCase):
     recipe = None
 
     def setUp(self):
-        from sparseml.transformers import SparseAutoModelForCausalLM, SparseAutoTokenizer
+        from sparseml.transformers import (
+            SparseAutoModelForCausalLM,
+            SparseAutoTokenizer,
+        )
+
         self.tokenizer = SparseAutoTokenizer.from_pretrained(self.model)
         self.model = SparseAutoModelForCausalLM.from_pretrained(self.model)
-        
+
         self.output = "./oneshot_output"
 
     def test_one_shot_inputs(self):
