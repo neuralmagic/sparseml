ee5c4a0eeca091a78f96113e9a40bf191cead4bb
Rahul Tuli <rahul@neuralmagic.com>
2024-05-23 13:58:20 +0000

    Missed commit

--- src/sparseml/modifiers/quantization/gptq/pytorch.py
+++ src/sparseml/modifiers/quantization/gptq/pytorch.py
@@ -117,13 +117,7 @@ class GPTQModifierPyTorch(GPTQModifier):
 
         for idx, (name, layer) in enumerate(self.compressible_layers_.items()):
             _LOGGER.info(f"Preparing {name} for compression")
-            if isinstance(self.sparsity, Dict):
-                layer_sparsity = self.sparsity[name]
-            elif isinstance(self.sparsity, List):
-                layer_sparsity = self.sparsity[idx]
-            else:  # float
-                layer_sparsity = self.sparsity
-            args = self._pruning_arguments(layer_sparsity)
+            args = self._pruning_arguments()
             comp_cls = self._compression_class()
             compressor = LayerCompressor(comp_cls, self.model, layer, idx, name, args)
             if not self.sequential_update:
