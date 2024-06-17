0828a6d19afb86cc98067b69240d6070fbe60257
Sara Adkins <sara@neuralmagic.com>
2024-06-17 20:20:41 +0000

    update output name

--- examples/llama7b_w8a8_quantization.py
+++ examples/llama7b_w8a8_quantization.py
@@ -37,7 +37,7 @@ model = SparseAutoModelForCausalLM.from_pretrained(
 dataset = "ultrachat-200k"
 
 # save location of quantized model out
-output_dir = "./output_llama7b_w8a8_channel_dynamic_compressed"
+output_dir = "./output_llama7b_w8a8_dynamic_compressed"
 
 # set dataset config parameters
 splits = {"calibration": "train_gen[:5%]"}
