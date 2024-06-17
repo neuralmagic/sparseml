d333c27120d064b0b7681f9f2637263d59f6074e
Sara Adkins <sara@neuralmagic.com>
2024-06-17 20:15:21 +0000

    cleanup

--- examples/llama7b_w8a8_quantization.py
+++ examples/llama7b_w8a8_quantization.py
@@ -16,18 +16,19 @@ quant_stage:
                         num_bits: 8
                         type: "int"
                         symmetric: true
+                        strategy: "channel"
                     input_activations:
                         num_bits: 8
                         type: "int"
-                        dynamic: true
                         symmetric: true
+                        dynamic: True
                         strategy: "token"
                     targets: ["Linear"]
 """
 
 # setting device_map to auto to spread the model evenly across all available GPUs
 # load the model in as bfloat16 to save on memory and compute
-model_stub = "TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T"
+model_stub = "zoo:llama2-7b-ultrachat200k_llama2_pretrain-base"
 model = SparseAutoModelForCausalLM.from_pretrained(
     model_stub, torch_dtype=torch.bfloat16, device_map="auto"
 )
@@ -36,7 +37,7 @@ model = SparseAutoModelForCausalLM.from_pretrained(
 dataset = "ultrachat-200k"
 
 # save location of quantized model out
-output_dir = "/network/sadkins/tinyllama-oneshot-w8a8-dynamic-token"
+output_dir = "./output_llama7b_w8a8_channel_dynamic_compressed"
 
 # set dataset config parameters
 splits = {"calibration": "train_gen[:5%]"}
